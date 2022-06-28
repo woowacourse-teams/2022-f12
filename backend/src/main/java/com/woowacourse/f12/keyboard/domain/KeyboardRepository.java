a8d6df339a5671d3cf5b46fa0a85df5882dbaa13
yangdongjue5510 <djwhy5510@gmail.com>
2022-06-28 15:51:35 +0900

    feat: 키보드 domain과 repository 생성

---
+++ backend/src/main/java/com/woowacourse/f12/keyboard/domain/KeyboardRepository.java
@@ -0,0 +1,7 @@
+package com.woowacourse.f12.keyboard.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KeyboardRepository extends JpaRepository<Keyboard, Long> {
+
+}
