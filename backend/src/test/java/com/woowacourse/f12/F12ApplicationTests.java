2a8d9f5a180a13df6718af085aab0411ceb0eb95
Jihoon Oh <66253212+Ohzzi@users.noreply.github.com>
2022-06-28 12:50:14 +0900

    [BE] 백엔드 프로젝트를 생성한다. (#2)
    * chore: 백엔드 프로젝트 생성
    
    * chore: property 설정
    
    * chore: PR 템플릿 오류 수정

---
+++ backend/src/test/java/com/woowacourse/f12/F12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowacourse.f12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
