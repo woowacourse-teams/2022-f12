38a1159e09aa8bfc6dd30af739a5e660cd9d1b62
yangdongjue5510 <djwhy5510@gmail.com>
2022-06-30 21:55:05 +0900

    feat: 특정 제품의 리뷰 목록을 정렬 및 페이징하여 조회하는 기능 구현

---
+++ backend/src/test/java/com/woowacourse/f12/domain/ReviewRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.woowacourse.f12.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.data.domain.Sort.Order.desc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
+
+@DataJpaTest
+class ReviewRepositoryTest {
+
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+    @Test
+    void 특정_제품의_리뷰_목록을_페이징하여_조회한다() {
+        // given
+        Long productId = 1L;
+        Pageable pageable = PageRequest.of(0, 1, Sort.by(desc("createdAt")));
+        Review review1 = reviewRepository.save(리뷰_저장(1L, "내용1", 5));
+        Review review2 = reviewRepository.save(리뷰_저장(1L, "내용2", 5));
+
+        // when
+        Slice<Review> page = reviewRepository.findPageByProductId(productId, pageable);
+
+        // then
+        assertAll(
+                () -> assertThat(page).hasSize(1)
+                        .extracting("id")
+                        .containsOnly(review2.getId()),
+                () -> assertThat(page.hasNext()).isTrue()
+        );
+    }
+
+    private Review 리뷰_저장(Long productId, String content, int rating) {
+        return Review.builder()
+                .productId(productId)
+                .content(content)
+                .rating(rating)
+                .build();
+    }
+}
