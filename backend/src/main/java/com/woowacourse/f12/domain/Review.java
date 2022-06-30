a9f1cbe8421cea604492bad7bdc96b4d837ab62b
yangdongjue5510 <djwhy5510@gmail.com>
2022-06-30 20:26:00 +0900

    feat: 리뷰 도메인 생성

---
+++ backend/src/main/java/com/woowacourse/f12/domain/Review.java
@@ -0,0 +1,58 @@
+package com.woowacourse.f12.domain;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Table(name = "review")
+@Getter
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "rating")
+    private int rating;
+
+    protected Review() {
+    }
+
+    @Builder
+    private Review(final Long id, final Long productId, final String content, final int rating) {
+        this.id = id;
+        this.productId = productId;
+        this.content = content;
+        this.rating = rating;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Review review = (Review) o;
+        return Objects.equals(id, review.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
