2221575b36e58da448dc8262631c05c04b3b37e3
yangdongjue5510 <djwhy5510@gmail.com>
2022-07-01 17:04:37 +0900

    refactor: application 클래스에서 Jpa 설정 분리

--- backend/src/main/java/com/woowacourse/f12/F12Application.java
+++ backend/src/main/java/com/woowacourse/f12/F12Application.java
@@ -2,14 +2,11 @@ package com.woowacourse.f12;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class F12Application {
 
     public static void main(String[] args) {
         SpringApplication.run(F12Application.class, args);
     }
-
 }
--- backend/src/test/java/com/woowacourse/f12/F12ApplicationTests.java
+++ backend/src/test/java/com/woowacourse/f12/F12ApplicationTests.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class F12ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
