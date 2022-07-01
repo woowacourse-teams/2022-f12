110be661c33193db0dc73fa55cd9888d26f211ad
Yo Wook Kim <uk960214@gmail.com>
2022-07-01 21:26:52 +0900

    chore: svg 활용을 위한 svgr 패키지 설치 및 설정
    Co-authored-by: Youngmin Hong <jswith@users.noreply.github.com>

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,12 +1,12 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
-  mode: process.env.production === "true" ? "production" : "development",
+  entry: './src/index.js',
+  mode: process.env.production === 'true' ? 'production' : 'development',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "main.[hash].js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.[hash].js',
     clean: true,
   },
   module: {
@@ -15,49 +15,54 @@ module.exports = {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
           },
         },
       },
       {
         test: /\.s?css$/,
         use: [
-          { loader: "style-loader" },
+          { loader: 'style-loader' },
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               modules: true,
             },
           },
-          { loader: "sass-loader" },
+          { loader: 'sass-loader' },
         ],
       },
       {
-        test: /\.(png|jpe?g|gif|svg|ico)$/,
-        type: "asset/resource",
+        test: /\.(png|jpe?g|gif|ico)$/,
+        type: 'asset/resource',
         generator: {
-          filename: "images/[hash][ext][query]",
+          filename: 'images/[hash][ext][query]',
         },
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "./public/index.html"),
+      template: path.resolve(__dirname, './public/index.html'),
     }),
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, "public"),
+      directory: path.join(__dirname, 'public'),
     },
     historyApiFallback: true,
     compress: true,
     port: 3000,
   },
   resolve: {
-    extensions: [".js", ".ts", ".jsx", ".tsx"],
+    extensions: ['.js', '.ts', '.jsx', '.tsx'],
     alias: {
       // 추후 사용
       // Components: path.resolve(__dirname, "./src/components"),
