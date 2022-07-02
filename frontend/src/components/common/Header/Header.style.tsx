6c5ce61e0d1df5b520fe6db73cc275c3c98e9c71
Yo Wook Kim <uk960214@gmail.com>
2022-07-02 19:21:50 +0900

    style: 헤더 컴포넌트를 sticky로 변경
    Co-authored-by: Youngmin Hong <jswith@users.noreply.github.com>

--- frontend/src/components/common/Header/Header.style.tsx
+++ frontend/src/components/common/Header/Header.style.tsx
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
 export const Container = styled.header`
+  position: sticky;
   box-shadow: 0 5px 5px -5px #00000099;
+  background-color: #ffffff;
   padding: 0.5rem 2rem;
+  top: 0;
 `;
 
 export const LogoImage = styled.img``;
