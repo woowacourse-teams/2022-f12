44a117a56820b5b73407e5fb6c040c01f556e8d1
Yo Wook Kim <uk960214@gmail.com>
2022-07-02 17:39:51 +0900

    feat: 헤더 컴포넌트 구현 및 스토리 작성
    Co-authored-by: Youngmin Hong <jswith@users.noreply.github.com>

---
+++ frontend/src/components/common/Header/Header.style.tsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  box-shadow: 0 5px 5px -5px #00000099;
+  padding: 0.5rem 2rem;
+`;
+
+export const LogoImage = styled.img``;
