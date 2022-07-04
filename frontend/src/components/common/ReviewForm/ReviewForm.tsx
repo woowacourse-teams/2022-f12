07850a5f01bac8e5dc4af92c33aadd9c00b69fe1
Yo Wook Kim <uk960214@gmail.com>
2022-07-04 17:01:49 +0900

    feat: 리뷰 폼 컴포넌트에 평점 입력 컴포넌트 추가
    Co-authored-by: Youngmin Hong <jswith@users.noreply.github.com>

--- frontend/src/components/common/ReviewForm/ReviewForm.tsx
+++ frontend/src/components/common/ReviewForm/ReviewForm.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
+import RatingInput from '../RatingInput/RatingInput';
 import * as S from './ReviewForm.style';
 
 function ReviewForm() {
+  const [rating, setRating] = useState<number>(0);
+
   return (
     <S.Container>
       <S.Title>리뷰 작성하기</S.Title>
       <S.Form>
+        <RatingInput rating={rating} setRating={setRating} />
         <S.Textarea />
         <S.SubmitButton>리뷰 추가</S.SubmitButton>
       </S.Form>
