6e575ef41c1c44ad6b997bdfeef18414c4cb9b98
Yo Wook Kim <uk960214@gmail.com>
2022-07-04 21:47:42 +0900

    [FE] API를 mocking한다 (#30)
    * chore: msw 환경 설정
    
    Co-authored-by: Youngmin Hong <jswith@users.noreply.github.com>
    
    * feat: API Mocking 구현
    
    Co-authored-by: Youngmin Hong <jswith@users.noreply.github.com>
    
    Co-authored-by: Youngmin Hong <jswith@users.noreply.github.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,15 @@ import { BrowserRouter } from 'react-router-dom';
 import ResetCss from './style/ResetCss';
 import App from './App';
 
+/* eslint-disable */
+
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
+/* eslint-enable */
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
