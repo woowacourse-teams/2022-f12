132f41c9a9617021b45becbec5aea25e0dc0916b
yangdongjue5510 <djwhy5510@gmail.com>
2022-07-04 18:11:41 +0900

    feat: 리뷰 내용 길이 검증 기능 추가

--- backend/src/test/java/com/woowacourse/f12/domain/ReviewTest.java
+++ backend/src/test/java/com/woowacourse/f12/domain/ReviewTest.java
@@ -3,7 +3,9 @@ package com.woowacourse.f12.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.woowacourse.f12.exception.InvalidContentLengthException;
 import com.woowacourse.f12.exception.InvalidRatingValueException;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -15,6 +17,7 @@ class ReviewTest {
         // given, when
         Review review = Review.builder()
                 .rating(rating)
+                .content("내용")
                 .build();
 
         // then
@@ -25,11 +28,38 @@ class ReviewTest {
     @ValueSource(ints = {0, 6})
     void 리뷰_평점_값이_1과_5_사이가_아니면_예외가_발생한다(int rating) {
         // given, when, then
-        assertThatThrownBy(
-                () -> Review.builder()
-                        .rating(rating)
-                        .build()
-        )
+        assertThatThrownBy(() -> Review.builder()
+                .rating(rating)
+                .content("내용")
+                .build())
                 .isExactlyInstanceOf(InvalidRatingValueException.class);
+    }
+
+    @Test
+    void 리뷰_내용의_길이가_1000자_이하면_생성된다() {
+        // given
+        String invalidContent = "1".repeat(1000);
+
+        // when
+        Review review = Review.builder()
+                .rating(5)
+                .content(invalidContent)
+                .build();
+
+        // then
+        assertThat(review).isNotNull();
+    }
+
+    @Test
+    void 리뷰_내용의_길이가_1000자가_넘으면_예외가_발생한다() {
+        // given
+        String invalidContent = "1".repeat(1001);
+
+        // when, then
+        assertThatThrownBy(() -> Review.builder()
+                .rating(5)
+                .content(invalidContent)
+                .build())
+                .isExactlyInstanceOf(InvalidContentLengthException.class);
     }
 }
