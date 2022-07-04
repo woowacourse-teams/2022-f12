d4afb46d6a1319ff3655216a8a0fd56b1b5c0d2d
Yo Wook Kim <uk960214@gmail.com>
2022-07-04 16:04:38 +0900

    feat: select-option 컴포넌트 구현
    Co-authored-by: Youngmin Hong <jswith@users.noreply.github.com>

---
+++ frontend/src/components/common/Select/Select.style.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.select`
+  width: 150px;
+  border: none;
+  border-bottom: 1px solid black;
+  padding: 10px;
+`;
+
+export const Option = styled.option``;
---
+++ frontend/src/components/common/Select/Select.tsx
@@ -0,0 +1,34 @@
+import React, { useMemo } from 'react';
+import * as S from './Select.style';
+
+type Props = {
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  options: { value: string; text: string }[];
+};
+
+function Select({ value, setValue, options }: Props) {
+  const handleOptionChange: React.ChangeEventHandler<HTMLSelectElement> = ({
+    target: { value },
+  }) => {
+    setValue(value);
+  };
+
+  const optionComponents = useMemo(
+    () =>
+      options.map(({ value, text }) => (
+        <S.Option key={value} value={value}>
+          {text}
+        </S.Option>
+      )),
+    [options]
+  );
+
+  return (
+    <S.Container value={value} onChange={handleOptionChange}>
+      {optionComponents}
+    </S.Container>
+  );
+}
+
+export default Select;
