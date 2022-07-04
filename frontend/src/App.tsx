0103f21dbbd918ff9a8fa2c98de96a3cc47c0095
Yo Wook Kim <uk960214@gmail.com>
2022-07-04 17:37:06 +0900

    feat: 전체 상품 페이지 컴포넌트 구현
    Co-authored-by: Youngmin Hong <jswith@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import Home from './pages/Home/Home';
+import Products from './pages/Products/Products';
 
 function App() {
   return (
     <div>
       <Home />
+      <Products />
     </div>
   );
 }
---
+++ frontend/src/pages/Products/mockData.ts
@@ -0,0 +1,24 @@
+import sampleKeyboard from './sample_keyboard.jpg';
+
+export const products = [
+  { id: 1, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 2, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 3, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 4, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 5, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 6, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 7, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 8, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 9, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 10, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 11, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 12, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 13, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 14, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 15, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 16, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 17, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 18, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 19, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+  { id: 20, name: '예쁜 키보드', productImage: sampleKeyboard, rating: 5 },
+];
