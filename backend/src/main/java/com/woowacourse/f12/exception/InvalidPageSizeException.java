fe7f6cbef75f7ffb6be897c60f19361ab45d9ba8
yangdongjue5510 <djwhy5510@gmail.com>
2022-07-05 15:22:34 +0900

    refactor: 예외 계층 구조 분리

--- backend/src/main/java/com/woowacourse/f12/exception/InvalidPageSizeException.java
+++ backend/src/main/java/com/woowacourse/f12/exception/InvalidPageSizeException.java
@@ -1,6 +1,6 @@
 package com.woowacourse.f12.exception;
 
-public class InvalidPageSizeException extends IllegalArgumentException {
+public class InvalidPageSizeException extends InvalidValueException {
 
     public InvalidPageSizeException(int maxSize) {
         super("페이지의 크기는" + maxSize + "이하여야 합니다.");
---
+++ backend/src/main/java/com/woowacourse/f12/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.woowacourse.f12.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(final String message) {
+        super(message);
+    }
+}
