9289045e1ae2b5329aefc07a55b6ce12a6c46469
Yo Wook Kim <uk960214@gmail.com>
2022-07-07 19:40:59 +0900

    refactor: Masonry 컴포넌트 타입 정리

--- frontend/src/components/common/Masonry/Masonry.tsx
+++ frontend/src/components/common/Masonry/Masonry.tsx
@@ -5,16 +5,15 @@ import * as S from './Masonry.style';
 type Props = { columnCount: number; children: React.ReactNode };
 
 function Masonry({ columnCount, children }: Props) {
-  const columns = Array.from({ length: columnCount }).reduce(
-    (acc) => [...acc, []],
-    []
-  );
+  const columnArray = Array.from({
+    length: columnCount,
+  }).reduce<React.ReactNode[][]>((acc) => [...acc, []], []);
 
-  React.Children.forEach(children, (child: React.ReactNode, index) => {
-    columns[index % columnCount].push(child);
+  React.Children.forEach(children, (child, index) => {
+    columnArray[index % columnCount].push(child);
   });
 
-  const childColumns = columns.map((column: React.ReactNode[], index) => {
+  const childColumns = columnArray.map((column: React.ReactNode[], index) => {
     return <S.Column key={index}>{column}</S.Column>;
   });
 
