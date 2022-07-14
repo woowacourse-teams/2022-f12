fb28412c5897e6602e00648c4fdd34d829cf0331
jswith <typingjs@gmail.com>
2022-07-14 23:49:04 +0900

    style: Stepper Progress 상태 UI 구현

--- frontend/src/components/common/Stepper/Stepper.style.ts
+++ frontend/src/components/common/Stepper/Stepper.style.ts
@@ -6,7 +6,7 @@ export const Container = styled.div`
   width: 40rem;
 `;
 
-export const Item = styled.div`
+export const Item = styled.div<{ step: number }>`
   position: relative;
   display: flex;
   flex-direction: column;
@@ -16,7 +16,7 @@ export const Item = styled.div`
   &::before {
     position: absolute;
     content: '';
-    border-bottom: 4px solid #cfcfcf;
+    /* border: 2px solid #cfcfcf; */
     width: 100%;
     top: 25px;
     left: -50%;
@@ -26,7 +26,7 @@ export const Item = styled.div`
   &::after {
     position: absolute;
     content: '';
-    border-bottom: 4px solid #cfcfcf;
+    border: 2px solid #cfcfcf;
     width: 100%;
     top: 25px;
     left: 50%;
@@ -36,8 +36,52 @@ export const Item = styled.div`
   &:first-child::before {
     content: none;
   }
+
+  &:first-child::after {
+    ${(props) => {
+      if (props.step === 2 || props.step === 3)
+        return `border: 2px solid #f6bebe;`;
+    }};
+  }
+
   &:last-child::after {
     content: none;
+  }
+
+  &:nth-child(2)::after {
+    ${(props) => {
+      if (props.step === 3) return `border: 2px solid #f6bebe;`;
+    }};
+  }
+
+  &:first-child {
+    .step {
+      ${(props) => {
+        if (props.step === 1) return `border: 5px solid #f6bebe;`;
+        if (props.step === 2 || props.step === 3)
+          return `border: 5px solid #f6bebe;
+          background-color: #f6bebe`;
+      }};
+    }
+  }
+
+  &:nth-child(2) {
+    .step {
+      ${(props) => {
+        if (props.step === 2) return `border: 5px solid #f6bebe;`;
+        if (props.step === 3)
+          return `border: 5px solid #f6bebe;
+        background-color: #f6bebe`;
+      }};
+    }
+  }
+
+  &:last-child {
+    .step {
+      ${(props) => {
+        if (props.step === 3) return `border: 5px solid #f6bebe`;
+      }};
+    }
   }
 `;
 
@@ -50,7 +94,6 @@ export const Step = styled.div`
   width: 50px;
   height: 50px;
   border-radius: 50%;
-  /* border: 5px solid #f6bebe; */
   border: 5px solid #cfcfcf;
   background-color: #faf9f9;
   margin-bottom: 6px;
--- frontend/src/components/common/Stepper/Stepper.tsx
+++ frontend/src/components/common/Stepper/Stepper.tsx
@@ -7,16 +7,16 @@ type Prop = {
 function Stepper({ step }: Prop) {
   return (
     <S.Container>
-      <S.Item>
-        <S.Step>1</S.Step>
+      <S.Item step={step}>
+        <S.Step className="step">1</S.Step>
         <S.Title>{step === 1 && '연차 입력'}</S.Title>
       </S.Item>
-      <S.Item>
-        <S.Step>2</S.Step>
+      <S.Item step={step}>
+        <S.Step className="step">2</S.Step>
         <S.Title>{step === 2 && '직군 입력'}</S.Title>
       </S.Item>
-      <S.Item>
-        <S.Step>3</S.Step>
+      <S.Item step={step}>
+        <S.Step className="step">3</S.Step>
         <S.Title>{step === 3 && '정보 확인'}</S.Title>
       </S.Item>
     </S.Container>
