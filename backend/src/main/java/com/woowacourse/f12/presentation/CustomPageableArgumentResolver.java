1476230ba8b88b3a92208c5e8362e5df0e6a6f4a
Jihoon Oh <66253212+Ohzzi@users.noreply.github.com>
2022-07-16 17:20:11 +0900

    [BE] Interceptor와 Resolver를 스프링 빈으로 만들어 한 번에 주입받아 등록하도록 변경 (#117)

--- backend/src/main/java/com/woowacourse/f12/presentation/CustomPageableArgumentResolver.java
+++ backend/src/main/java/com/woowacourse/f12/presentation/CustomPageableArgumentResolver.java
@@ -9,10 +9,12 @@ import java.util.regex.Pattern;
 import org.springframework.core.MethodParameter;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Component
 public class CustomPageableArgumentResolver extends PageableHandlerMethodArgumentResolver {
 
     private static final int MAX_SIZE = 150;
