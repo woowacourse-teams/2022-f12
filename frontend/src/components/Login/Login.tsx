ded3bac20f3cec3c0817ff185497960818a7f068
Yo Wook Kim <uk960214@gmail.com>
2022-07-17 14:49:36 +0900

    fix: 임시로 로그인 시 무조건 홈 페이지로 넘어가도록 수정

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,6 +1,6 @@
 import useLogin from '@/hooks/useLogin';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+// import { useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 
 const params = new URLSearchParams(location.search);
 const code = params.get('code');
@@ -8,19 +8,12 @@ export const authURI = `http://localhost:8080/api/v1/login`;
 
 function Login() {
   const userData = useLogin(authURI, code);
-  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (userData?.accessToken) {
-      if (userData?.jobType === null || userData?.career === null) {
-        navigate('/register');
-      } else {
-        navigate('/');
-      }
-    }
-  }, [userData]);
-
-  return <div>로그인 진행중..</div>;
+  return userData && userData.accessToken ? (
+    <Navigate to={'/'} />
+  ) : (
+    <div>로그인 진행중..</div>
+  );
 }
 
 export default Login;
