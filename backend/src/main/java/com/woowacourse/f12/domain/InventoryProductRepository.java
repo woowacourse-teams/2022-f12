122dd7f4bf8b42b5011cb6e37d68ed66e548adf1
yh20studio(tiki) <yh20studio@gmail.com>
2022-07-18 21:53:10 +0900

    feat: InventoryProduct 엔티티 구현

---
+++ backend/src/main/java/com/woowacourse/f12/domain/InventoryProductRepository.java
@@ -0,0 +1,7 @@
+package com.woowacourse.f12.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryProductRepository extends JpaRepository<InventoryProduct, Long> {
+
+}
