2c838d51c5041b0ec44231fd4788b43d214a262b
Yo Wook Kim <uk960214@gmail.com>
2022-07-18 14:55:09 +0900

    fix: 로그인 시 code 접근 훅을 params에서 searchParam으로 수정

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,16 +1,16 @@
 import ROUTES from '@/constants/routes';
 import useAuth from '@/hooks/useAuth';
 import { useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 function Login() {
   const { login } = useAuth();
-  const { code } = useParams();
+  const [searchParam] = useSearchParams();
 
   const navigate = useNavigate();
 
   useEffect(() => {
-    login(code)
+    login(searchParam.get('code'))
       .catch(() => {
         alert('로그인에 실패했습니다. 잠시 후 다시 시도해주세요.');
       })
