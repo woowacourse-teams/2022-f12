f8f30dc8d773289a40539f575a044df711be62d6
Youngmin Hong <typingjs@gmail.com>
2022-07-21 16:09:29 +0900

    [FE] 스프린트 2 데모를 위한 UI 마무리 (#205)
    * feat: 전체 상품 페이지의 정렬 기준에서 default를 제거한다.
    
    * feat: 상단 nav에 전체 상품 페이지, 인기 상품 페이지로 연결하는 링크를 추가한다

--- frontend/src/pages/Products/Products.tsx
+++ frontend/src/pages/Products/Products.tsx
@@ -1,13 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import ProductListSection from '@/components/ProductListSection/ProductListSection';
 import Select from '@/components/common/Select/Select';
 import useProducts from '@/hooks/useProducts';
+import { useLocation } from 'react-router-dom';
 
 type Option = { value: string; text: string };
 
-type Sort = 'default' | 'rating,desc' | 'reviewCount,desc';
-type SortText = '기본 순' | '평점 높은 순' | '리뷰 많은 순';
+type Sort = 'rating,desc' | 'reviewCount,desc';
+type SortText = '평점 높은 순' | '리뷰 많은 순';
 
 interface ProductSortOption extends Option {
   value: Sort;
@@ -15,23 +16,30 @@ interface ProductSortOption extends Option {
 }
 
 const options: ProductSortOption[] = [
-  { value: 'default', text: '기본 순' },
   { value: 'rating,desc', text: '평점 높은 순' },
   { value: 'reviewCount,desc', text: '리뷰 많은 순' },
 ];
 
+const PopularSort = options[0];
 const DefaultSort = options[1];
 
 function Products() {
+  const location = useLocation();
   const [sort, setSort] = useState<Sort>(DefaultSort.value);
   const [keyboards, getNextPage] = useProducts({
     size: 12,
     sort,
   });
 
+  useEffect(() => {
+    setSort(
+      location.hash === '#popular' ? PopularSort.value : DefaultSort.value
+    );
+  }, [location.hash]);
+
   return (
     <ProductListSection
-      title={'모든 상품 목록'}
+      title={location.hash === '#popular' ? '인기 상품 목록' : '모든 상품 목록'}
       data={!!keyboards && keyboards}
       getNextPage={getNextPage}
       addOn={<Select value={sort} setValue={setSort} options={options} />}
