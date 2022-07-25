1667ffcd156082a6d4c54580299a136931f948ad
Yo Wook Kim <uk960214@gmail.com>
2022-07-25 22:00:29 +0900

    chore: favicon이 webpack으로 함께 bundle 되도록 설정
    Co-authored-by: Youngmin Hong <jswith@users.noreply.github.com>

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -46,6 +46,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './public/index.html'),
+      favicon: path.resolve(__dirname, './public/favicon.ico'),
     }),
   ],
   resolve: {
--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,13 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
 
 module.exports = merge(common, {
   mode: 'production',
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, './public/index.html'),
-    }),
-  ],
 });
