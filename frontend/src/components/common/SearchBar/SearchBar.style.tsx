f06380cd402e65999a9306c588bffa388a9621e3
jswith <typingjs@gmail.com>
2022-07-27 20:15:49 +0900

    feat: 검색바 컴포넌트 UI 구현

---
+++ frontend/src/components/common/SearchBar/SearchBar.style.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 60%;
+  display: flex;
+  align-items: center;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 2.8rem;
+  background: #ffffff;
+  outline: none;
+  border: none;
+  border-radius: 1.625rem;
+  padding: 0 3.5rem 0 1.5rem;
+  font-size: 1rem;
+`;
+
+export const Button = styled.button`
+  width: 3.5rem;
+  height: 2.8rem;
+  margin-left: -3.5rem;
+  background: none;
+  border: none;
+  outline: none;
+`;
