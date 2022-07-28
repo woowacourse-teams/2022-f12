5491c78d524cbac54ebbae6d5ffa351b385a979b
yh20studio(tiki) <yh20studio@gmail.com>
2022-07-28 21:44:58 +0900

    docs: 제품 사용자 통계 조회 API 문서화
    Co-authored-by: hamcheeseburger <yoo77hyeon@gmail.com>
    Co-authored-by: yangdongjue5510 <djwhy5510@gmail.com>

--- backend/src/test/java/com/woowacourse/f12/documentation/product/ProductDocumentation.java
+++ backend/src/test/java/com/woowacourse/f12/documentation/product/ProductDocumentation.java
@@ -1,8 +1,17 @@
 package com.woowacourse.f12.documentation.product;
 
+import static com.woowacourse.f12.domain.member.CareerLevel.JUNIOR;
+import static com.woowacourse.f12.domain.member.CareerLevel.MID_LEVEL;
+import static com.woowacourse.f12.domain.member.CareerLevel.NONE;
+import static com.woowacourse.f12.domain.member.CareerLevel.SENIOR;
+import static com.woowacourse.f12.domain.member.JobType.BACKEND;
+import static com.woowacourse.f12.domain.member.JobType.ETC;
+import static com.woowacourse.f12.domain.member.JobType.FRONTEND;
+import static com.woowacourse.f12.domain.member.JobType.MOBILE;
 import static com.woowacourse.f12.presentation.product.CategoryConstant.KEYBOARD_CONSTANT;
 import static com.woowacourse.f12.support.ProductFixture.KEYBOARD_1;
 import static com.woowacourse.f12.support.ProductFixture.KEYBOARD_2;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -11,11 +20,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.woowacourse.f12.application.product.ProductService;
 import com.woowacourse.f12.documentation.Documentation;
+import com.woowacourse.f12.domain.member.CareerLevel;
+import com.woowacourse.f12.domain.member.JobType;
 import com.woowacourse.f12.domain.product.Product;
 import com.woowacourse.f12.dto.response.product.ProductPageResponse;
 import com.woowacourse.f12.dto.response.product.ProductResponse;
+import com.woowacourse.f12.dto.response.product.ProductStatisticsResponse;
 import com.woowacourse.f12.presentation.product.ProductController;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -77,5 +90,26 @@ class ProductDocumentation extends Documentation {
                 .andDo(
                         document("products-page-get")
                 );
+    }
+
+    @Test
+    void 특정_제품에_대한_사용자_통계_조회_API_문서화() throws Exception {
+        // given
+        Map<CareerLevel, Double> careerLevel = Map.of(NONE, 0.0, JUNIOR, 0.5,
+                MID_LEVEL, 0.0, SENIOR, 0.5);
+        Map<JobType, Double> jobType = Map.of(FRONTEND, 0.5, BACKEND,
+                0.5, MOBILE, 0.0, ETC, 0.0);
+        given(productService.calculateMemberStatisticsById(anyLong()))
+                .willReturn(ProductStatisticsResponse.of(careerLevel, jobType));
+
+        // when
+        ResultActions resultActions = mockMvc.perform(
+                get("/api/v1/products/1/statistics")
+        );
+
+        // then
+        resultActions.andExpect(status().isOk())
+                .andDo(document("products-member-statistics-get"))
+                .andDo(print());
     }
 }
