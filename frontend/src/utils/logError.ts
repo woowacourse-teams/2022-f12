0b8e0ce307b17e5ef6a219fe431a8fe999982ea5
Yo Wook Kim <uk960214@gmail.com>
2022-07-29 19:37:54 +0900

    feat: 나머지 API 훅에 대한 오류 처리 구현

--- frontend/src/utils/logError.ts
+++ frontend/src/utils/logError.ts
@@ -1,4 +1,4 @@
-const logError = (error: Error, additionalMessage) => {
+const logError = (error: Error, additionalMessage?: string) => {
   console.error(error.message, '\n', additionalMessage);
   console.groupCollapsed('실제 오류');
   console.error(error.stack);
