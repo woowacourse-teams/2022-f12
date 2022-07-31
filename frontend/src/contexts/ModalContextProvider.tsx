c2bae8c3e6852fb09e02c5c76575506df4733582
Yo Wook Kim <uk960214@gmail.com>
2022-07-31 22:12:50 +0900

    feat: modal 렌더링 컨텍스트 구현

---
+++ frontend/src/contexts/ModalContextProvider.tsx
@@ -0,0 +1,60 @@
+import Modal from '@/components/common/Modal/Modal';
+import { createContext, PropsWithChildren, useState } from 'react';
+
+export const ShowAlertContext = createContext<(message: string) => void>(null);
+export const GetConfirmContext =
+  createContext<(message: string) => Promise<boolean>>(null);
+
+let resolveConfirm: (value: boolean | PromiseLike<boolean>) => void;
+function ModalContextProvider({ children }: PropsWithChildren) {
+  const [message, setMessage] = useState('');
+  const [alertOpen, setAlertOpen] = useState(false);
+  const [confirmOpen, setConfirmOpen] = useState(false);
+
+  const showAlert = (message: string) => {
+    setMessage(message);
+    setAlertOpen(true);
+  };
+  const showConfirm = (message: string) => {
+    setMessage(message);
+    setConfirmOpen(true);
+  };
+
+  const handleClose = () => {
+    setAlertOpen(false);
+    setConfirmOpen(false);
+  };
+
+  const handleConfirm = () => {
+    resolveConfirm(true);
+    setConfirmOpen(false);
+  };
+
+  const getConfirm: (message: string) => Promise<boolean> = (message) => {
+    showConfirm(message);
+
+    return new Promise((resolve) => {
+      resolveConfirm = resolve;
+    });
+  };
+
+  return (
+    <ShowAlertContext.Provider value={showAlert}>
+      <GetConfirmContext.Provider value={getConfirm}>
+        {children}
+        {alertOpen && (
+          <Modal handleClose={handleClose}>
+            <Modal.Body>{message}</Modal.Body>
+          </Modal>
+        )}
+        {confirmOpen && (
+          <Modal handleClose={handleClose} handleConfirm={handleConfirm}>
+            <Modal.Body>{message}</Modal.Body>
+          </Modal>
+        )}
+      </GetConfirmContext.Provider>
+    </ShowAlertContext.Provider>
+  );
+}
+
+export default ModalContextProvider;
