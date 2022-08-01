f34cfb56e4e1f44f7a607986549f32f1fc2cbb4b
Youngmin Hong <typingjs@gmail.com>
2022-08-01 18:58:37 +0900

    [Feature/FE] 사용자 통계 기능 구현 (#310)
    [FE] 사용자 통계 기능 구현 (#309)
    
    * feat: 사용자 통계 기능 구현
    
    * fix: BarGraph storybook 에러 수정

--- frontend/src/components/common/BarGraph/BarGraph.stories.tsx
+++ frontend/src/components/common/BarGraph/BarGraph.stories.tsx
@@ -6,6 +6,23 @@ export default {
   title: 'Components/BarGraph',
 };
 
-const Template: ComponentStory<typeof BarGraph> = () => <BarGraph />;
+const Template: ComponentStory<typeof BarGraph> = (args) => (
+  <BarGraph {...args} />
+);
 
-export const Default = () => <Template />;
+const statistics = {
+  careerLevel: {
+    midlevel: 0.2,
+    senior: 0.3,
+    none: 0.1,
+    junior: 0.4,
+  },
+  jobType: {
+    frontend: 0.45,
+    backend: 0.25,
+    mobile: 0.2,
+    etc: 0.1,
+  },
+};
+
+export const Default = () => <Template statistics={statistics} />;
