169d3bc87c1ecb2d9023ca8f925b02c0724675ca
Yo Wook Kim <uk960214@gmail.com>
2022-08-02 15:32:44 +0900

    chore: cypress 설정 마무리

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
     baseUrl: 'http://localhost:3000',
     viewportHeight: 900,
     viewportWidth: 1440,
