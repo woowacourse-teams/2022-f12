4b797be2e3159d5f4d02628f2aebe096c994a957
Yo Wook Kim <uk960214@gmail.com>
2022-08-02 16:01:59 +0900

    test: 홈 페이지에 대한 E2E 테스트 작성

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,43 +1,46 @@
-import { ENDPOINTS, BASE_URL } from '../../src/constants/api';
-import { handlers } from '../../src/mocks/handlers';
-
-import { setupServer } from 'msw/node';
-
-const server = setupServer(...handlers);
-
-beforeAll(() => server.listen());
-afterAll(() => server.close());
-afterEach(() => server.resetHandlers());
-
 describe('비회원 사용자 기본 플로우', () => {
   beforeEach(() => {
-    // cy.intercept(`${ENDPOINTS.REVIEWS}`).as('reviewsRequest');
-    cy.visit('/');
-    cy.intercept('GET', '**/products*', {
-      statusCode: 201,
-      body: {
-        name: 'Peter Pan',
-      },
-    }).as('productsRequest');
-    // cy.visit('');
-  });
-  it('메인 페이지에 접속할 수 있다.', () => {
+    cy.intercept({ method: 'GET', url: '**/api/v1/products*' }).as(
+      'productsRequest'
+    );
+    cy.intercept({ method: 'GET', url: '**/api/v1/reviews*' }).as(
+      'reviewsRequest'
+    );
     cy.visit('');
-    cy.wait('@productsRequest');
-    // cy.intercept({ method: 'GET', url: '**/api/v1/*' }).as('productsRequest');
   });
 
-  // it('메인 페이지에 접속하면 인기 있는 상품을 조회할 수 있다.', () => {
-  //   cy.visit('');
-  //   // cy.intercept({ method: 'GET', url: '**/products/*' }).as('productsRequest');
-  //   cy.wait('@productsRequest');
-  // });
+  it('홈 페이지에 접속할 수 있다.', () => {
+    cy.visit('');
+  });
 
-  // it('메인 페이지에 접속하면 최근에 작성된 후기를 조회할 수 있다.', () => {
-  //   cy.visit('');
-  // });
+  it('홈 페이지에 접속하면 상품을 조회할 수 있다.', () => {
+    cy.wait('@productsRequest');
 
-  // it('메인 페이지에서 스크롤을 내리면 최근에 작성된 후기가 추가 로딩이 된다.', () => {
-  //   cy.visit('');
-  // });
+    cy.findByRole('region', { name: /상품/ })
+      .findAllByRole('article')
+      .should('be.visible');
+  });
+
+  it('홈 페이지에 접속하면 후기를 조회할 수 있다.', () => {
+    cy.wait('@reviewsRequest');
+
+    cy.findByRole('region', { name: /후기/ })
+      .findAllByRole('article')
+      .should('be.visible');
+  });
+
+  it('홈 페이지에서 스크롤을 내리면 후기가 추가 로딩이 된다.', () => {
+    cy.wait('@reviewsRequest');
+
+    cy.scrollTo('bottom');
+
+    cy.wait('@reviewsRequest').then((res) => {
+      const page = new URL(res.request.url).searchParams.get('page');
+      expect(page).to.equal('1');
+
+      cy.findByRole('region', { name: /후기/ })
+        .findByRole('region', { name: 'loading' })
+        .should('not.exist');
+    });
+  });
 });
