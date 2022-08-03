99f1c21e1d1d2063295e2bb907d48e5c853f0d54
jswith <typingjs@gmail.com>
2022-08-03 20:20:09 +0900

    feat: MSW 실행

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,21 +10,21 @@ import ModalContextProvider from '@/contexts/ModalContextProvider';
 
 /* eslint-disable */
 
-// if (process.env.NODE_ENV === 'development' && !window.Cypress) {
-//   const { worker } = require('@/mocks/browser');
-//   worker.start({
-//     onUnhandledRequest(req) {
-//       const urlPath = req.url.pathname;
+if (process.env.NODE_ENV === 'development' && !window.Cypress) {
+  const { worker } = require('@/mocks/browser');
+  worker.start({
+    onUnhandledRequest(req) {
+      const urlPath = req.url.pathname;
 
-//       if (!urlPath.startsWith('http://localhost:8080')) return;
-//       console.warn(
-//         'Found an unhandled %s request to %s',
-//         req.method,
-//         req.url.href
-//       );
-//     },
-//   });
-// }
+      if (!urlPath.startsWith('http://localhost:8080')) return;
+      console.warn(
+        'Found an unhandled %s request to %s',
+        req.method,
+        req.url.href
+      );
+    },
+  });
+}
 
 /* eslint-enable */
 
