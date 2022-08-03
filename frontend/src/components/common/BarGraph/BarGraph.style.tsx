2f3c5683b6bc00da8474ef51b5b1ed084cd07934
Yo Wook Kim <uk960214@gmail.com>
2022-08-03 14:02:16 +0900

    fix: 통계 UI 관련 테스트 수정 및 무한 스크롤 테스트 수정

--- frontend/src/components/common/BarGraph/BarGraph.style.tsx
+++ frontend/src/components/common/BarGraph/BarGraph.style.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.article`
+export const Container = styled.section`
   display: flex;
   position: relative;
   flex-direction: column;
