5a62288e39de7a7d8e02e3d0dc046428d4ddbf94
HyeonJi Yoo <61769743+hamcheeseburger@users.noreply.github.com>
2022-08-04 10:01:04 +0900

    [BugFix/BE] 회원 정보를 조회할 때 id에 대한 역순으로 정렬이 안되는 오류 수정 (#369)
    * [BE] 회원 정보를 조회할 때 id에 대한 역순으로 정렬이 안되는 오류 수정 (#368)
    
    * fix: 회원정보 조회 시 기본 정렬 기준이 적용되도록 수정
    
    * refactor: 접근제어자 수정
    
    * refactor: 불필요한 개행 제거

--- backend/src/main/java/com/woowacourse/f12/domain/member/MemberRepositoryCustomImpl.java
+++ backend/src/main/java/com/woowacourse/f12/domain/member/MemberRepositoryCustomImpl.java
@@ -1,16 +1,22 @@
 package com.woowacourse.f12.domain.member;
 
-import static com.woowacourse.f12.domain.member.QMember.member;
-
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.PathBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import java.util.List;
-import java.util.Objects;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.SliceImpl;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import static com.woowacourse.f12.domain.member.QMember.member;
 
 public class MemberRepositoryCustomImpl extends QuerydslRepositorySupport implements MemberRepositoryCustom {
 
@@ -24,16 +30,38 @@ public class MemberRepositoryCustomImpl extends QuerydslRepositorySupport implem
     public Slice<Member> findBySearchConditions(final String keyword, final CareerLevel careerLevel,
                                                 final JobType jobType,
                                                 final Pageable pageable) {
-        final JPAQuery<Member> jpaQuery = jpaQueryFactory.selectFrom(member)
+        final JPAQuery<Member> jpaQuery = jpaQueryFactory.select(member)
+                .from(member)
                 .where(
                         containsKeyword(keyword),
                         eqCareerLevel(careerLevel),
                         eqJobType(jobType)
                 )
                 .offset(pageable.getOffset())
-                .limit(pageable.getPageSize() + 1);
-        final List<Member> members = jpaQuery.fetch();
-        return toSlice(pageable, members);
+                .limit(pageable.getPageSize() + 1)
+                .orderBy(makeOrderSpecifiers(pageable));
+
+        return toSlice(pageable, jpaQuery.fetch());
+    }
+
+    private OrderSpecifier[] makeOrderSpecifiers(final Pageable pageable) {
+        return pageable.getSort()
+                .stream()
+                .map(this::toOrderSpecifier)
+                .collect(Collectors.toList()).toArray(OrderSpecifier[]::new);
+    }
+
+    private OrderSpecifier toOrderSpecifier(final Sort.Order sortOrder) {
+        final Order orderMethod = toOrder(sortOrder);
+        final PathBuilder<Member> pathBuilder = new PathBuilder<>(member.getType(), member.getMetadata());
+        return new OrderSpecifier(orderMethod, pathBuilder.get(sortOrder.getProperty()));
+    }
+
+    private Order toOrder(final Sort.Order sortOrder) {
+        if (sortOrder.isAscending()) {
+            return Order.ASC;
+        }
+        return Order.DESC;
     }
 
     private BooleanExpression containsKeyword(final String keyword) {
@@ -43,14 +71,14 @@ public class MemberRepositoryCustomImpl extends QuerydslRepositorySupport implem
         return member.gitHubId.contains(keyword);
     }
 
-    public BooleanExpression eqCareerLevel(final CareerLevel careerLevel) {
+    private BooleanExpression eqCareerLevel(final CareerLevel careerLevel) {
         if (Objects.isNull(careerLevel)) {
             return null;
         }
         return member.careerLevel.eq(careerLevel);
     }
 
-    public BooleanExpression eqJobType(final JobType jobType) {
+    private BooleanExpression eqJobType(final JobType jobType) {
         if (Objects.isNull(jobType)) {
             return null;
         }
@@ -62,7 +90,6 @@ public class MemberRepositoryCustomImpl extends QuerydslRepositorySupport implem
             members.remove(members.size() - 1);
             return new SliceImpl<>(members, pageable, true);
         }
-
         return new SliceImpl<>(members, pageable, false);
     }
 }
