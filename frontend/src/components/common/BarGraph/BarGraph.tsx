4be53180aedbd62ab5b2e0fd8919af6a4354380a
jswith <typingjs@gmail.com>
2022-08-09 14:35:25 +0900

    refactor: BarGraph 리팩토링

--- frontend/src/components/common/BarGraph/BarGraph.tsx
+++ frontend/src/components/common/BarGraph/BarGraph.tsx
@@ -1,141 +1,92 @@
+import { useState } from 'react';
+
 import * as S from '@/components/common/BarGraph/BarGraph.style';
 import theme from '@/style/theme';
-import { useState } from 'react';
 
 type Prop = {
   statistics: Statistics;
 };
 
+type JobType = ['프론트엔드', '백엔드', '모바일', '기타'];
+type CareerLevel = ['경력 없음', '0-2년차', '3-5년차', '6년차 이상'];
+
+const jobTypeList: JobType = ['프론트엔드', '백엔드', '모바일', '기타'];
+const careerLevelList: CareerLevel = [
+  '경력 없음',
+  '0-2년차',
+  '3-5년차',
+  '6년차 이상',
+];
+
 function BarGraph({ statistics }: Prop) {
   const [isJobType, setIsJobType] = useState(true);
+  const { careerLevel, jobType } = statistics;
+
+  const statisticsData = [
+    {
+      color: theme.colors.primary,
+      percent: Math.round(
+        (isJobType ? jobType.frontend : careerLevel.none) * 100
+      ),
+    },
+    {
+      color: theme.colors.primaryDark,
+      percent: Math.round(
+        (isJobType ? jobType.backend : careerLevel.junior) * 100
+      ),
+    },
+    {
+      color: theme.colors.secondary,
+      percent: Math.round(
+        (isJobType ? jobType.mobile : careerLevel.midlevel) * 100
+      ),
+    },
+    {
+      color: theme.colors.black,
+      percent: Math.round((isJobType ? jobType.etc : careerLevel.senior) * 100),
+    },
+  ];
 
   const toggleGraph = () => {
     setIsJobType((state) => !state);
   };
 
-  const { careerLevel, jobType } = statistics;
-
   return (
-    <>
-      {isJobType ? (
-        <S.Container aria-label="통계 정보">
-          <S.DataWrapper>
-            <S.BarWrapper>
+    <S.Container aria-label="통계 정보">
+      <S.DataWrapper>
+        {statisticsData.map((data, index) => {
+          return (
+            <S.BarWrapper key={index}>
               <S.Bar
-                key={0}
-                color={theme.colors.primary}
-                height={Math.round(jobType.frontend * 100)}
+                key={Math.random()}
+                color={data.color}
+                height={data.percent}
               />
               <S.PercentWrapper>
-                <S.Percent>{Math.round(jobType.frontend * 100)}%</S.Percent>
+                <S.Percent>{data.percent}%</S.Percent>
               </S.PercentWrapper>
             </S.BarWrapper>
-            <S.BarWrapper>
-              <S.Bar
-                key={1}
-                color={theme.colors.primaryDark}
-                height={Math.round(jobType.backend * 100)}
-              />
-              <S.PercentWrapper>
-                <S.Percent>{Math.round(jobType.backend * 100)}%</S.Percent>
-              </S.PercentWrapper>
-            </S.BarWrapper>
-            <S.BarWrapper>
-              <S.Bar
-                key={2}
-                color={theme.colors.secondary}
-                height={Math.round(jobType.mobile * 100)}
-              />
-              <S.PercentWrapper>
-                <S.Percent>{Math.round(jobType.mobile * 100)}%</S.Percent>
-              </S.PercentWrapper>
-            </S.BarWrapper>
-            <S.BarWrapper>
-              <S.Bar
-                key={3}
-                color={theme.colors.black}
-                height={Math.round(jobType.etc * 100)}
-              />
-              <S.PercentWrapper>
-                <S.Percent>{Math.round(jobType.etc * 100)}%</S.Percent>
-              </S.PercentWrapper>
-            </S.BarWrapper>
-          </S.DataWrapper>
-          <S.JobTypeWrapper>
-            {['프론트엔드', '백엔드', '모바일', '기타'].map(
-              (jobType, index) => {
-                return <S.JobType key={index}>{jobType}</S.JobType>;
-              }
-            )}
-          </S.JobTypeWrapper>
-          <S.BarGraphTitleWrapper>
-            <S.BarGraphTitle>직군별 통계</S.BarGraphTitle>
-            <S.BarGraphToggleButton onClick={toggleGraph}>
-              연차별 통계 보기
-            </S.BarGraphToggleButton>
-          </S.BarGraphTitleWrapper>
-        </S.Container>
-      ) : (
-        <S.Container aria-label="통계 정보">
-          <S.DataWrapper>
-            <S.BarWrapper>
-              <S.Bar
-                key={4}
-                color={theme.colors.primary}
-                height={Math.round(careerLevel.none * 100)}
-              />
-              <S.PercentWrapper>
-                <S.Percent>{Math.round(careerLevel.none * 100)}%</S.Percent>
-              </S.PercentWrapper>
-            </S.BarWrapper>
-            <S.BarWrapper>
-              <S.Bar
-                key={5}
-                color={theme.colors.primaryDark}
-                height={Math.round(careerLevel.junior * 100)}
-              />
-              <S.PercentWrapper>
-                <S.Percent>{Math.round(careerLevel.junior * 100)}%</S.Percent>
-              </S.PercentWrapper>
-            </S.BarWrapper>
-            <S.BarWrapper>
-              <S.Bar
-                key={6}
-                color={theme.colors.secondary}
-                height={Math.round(careerLevel.midlevel * 100)}
-              />
-              <S.PercentWrapper>
-                <S.Percent>{Math.round(careerLevel.midlevel * 100)}%</S.Percent>
-              </S.PercentWrapper>
-            </S.BarWrapper>
-            <S.BarWrapper>
-              <S.Bar
-                key={7}
-                color={theme.colors.black}
-                height={Math.round(careerLevel.senior * 100)}
-              />
-              <S.PercentWrapper>
-                <S.Percent>{Math.round(careerLevel.senior * 100)}%</S.Percent>
-              </S.PercentWrapper>
-            </S.BarWrapper>
-          </S.DataWrapper>
-          <S.JobTypeWrapper>
-            {['경력 없음', '0-2년차', '3-5년차', '5년차 이상'].map(
-              (jobType, index) => {
-                return <S.JobType key={index}>{jobType}</S.JobType>;
-              }
-            )}
-          </S.JobTypeWrapper>
-          <S.BarGraphTitleWrapper>
-            <S.BarGraphTitle>연차별 통계</S.BarGraphTitle>
-            <S.BarGraphToggleButton onClick={toggleGraph}>
-              직군별 통계 보기
-            </S.BarGraphToggleButton>
-          </S.BarGraphTitleWrapper>
-        </S.Container>
-      )}
-    </>
+          );
+        })}
+      </S.DataWrapper>
+      <S.JobTypeWrapper>
+        {isJobType
+          ? jobTypeList.map((title, index) => {
+              return <S.JobType key={index}>{title}</S.JobType>;
+            })
+          : careerLevelList.map((title, index) => {
+              return <S.JobType key={index}>{title}</S.JobType>;
+            })}
+      </S.JobTypeWrapper>
+      <S.BarGraphTitleWrapper>
+        <S.BarGraphTitle>
+          {isJobType ? '직군별 통계' : '연차별 통계'}
+        </S.BarGraphTitle>
+        <S.BarGraphToggleButton onClick={toggleGraph}>
+          {isJobType ? '연차별 통계 보기' : '직군별 통계 보기'}
+        </S.BarGraphToggleButton>
+      </S.BarGraphTitleWrapper>
+    </S.Container>
   );
 }
-
 export default BarGraph;
