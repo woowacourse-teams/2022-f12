dc21e1b8f5df0bfefe512adb5c233122f0561559
Ohzzi <clover7even@naver.com>
2022-08-09 13:17:43 +0900

    fix: CORS를 모든 origin에 대해 허용하던 것에서 특정 origin에만 허용하도록 수정

--- backend/src/test/java/com/woowacourse/f12/config/WebConfigTest.java
+++ backend/src/test/java/com/woowacourse/f12/config/WebConfigTest.java
@@ -6,6 +6,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,17 +23,29 @@ class WebConfigTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void CORS가_허용되어있다() throws Exception {
+    @ParameterizedTest
+    @ValueSource(strings = {"https://f12.app", "https://www.f12.app", "https://test.f12.app", "http://localhost:3000"})
+    void 특정_Origin에_CORS가_허용되어있다(String origin) throws Exception {
         mockMvc.perform(
                         options("/api/v1/products")
-                                .header(HttpHeaders.ORIGIN, "http://localhost:8080")
+                                .header(HttpHeaders.ORIGIN, origin)
                                 .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "GET")
                 )
                 .andExpect(status().isOk())
-                .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*"))
+                .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, origin))
                 .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, CORS_ALLOWED_METHODS))
                 .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, HttpHeaders.LOCATION))
+                .andDo(print());
+    }
+
+    @Test
+    void CORS가_허용되지_않은_Origin에서_Preflight_요청을_보내면_허용하지_않는다() throws Exception {
+        mockMvc.perform(
+                        options("/api/v1/products")
+                                .header(HttpHeaders.ORIGIN, "http://not-allowed-origin")
+                                .header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, "GET")
+                )
+                .andExpect(status().isForbidden())
                 .andDo(print());
     }
 }
