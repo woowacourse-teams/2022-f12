5611691681a782d4b364728a1acf2da92e29712d
jswith <typingjs@gmail.com>
2022-08-11 14:39:17 +0900

    refactor: 로그아웃 로직을 useAuth 훅으로 이동한다.
    Co-authored-by: Yo Wook Kim <uk960214@gmail.com>

--- frontend/src/components/common/HeaderNav/HeaderNav.tsx
+++ frontend/src/components/common/HeaderNav/HeaderNav.tsx
@@ -6,35 +6,21 @@ import * as S from '@/components/common/HeaderNav/HeaderNav.style';
 
 import useAnimation from '@/hooks/useAnimation';
 import useAuth from '@/hooks/useAuth';
-import useModal from '@/hooks/useModal';
 
 import { GITHUB_AUTH_URL } from '@/constants/api';
 import ROUTES from '@/constants/routes';
 
 function HeaderNav() {
-  const { logout, isLoggedIn } = useAuth();
-  const { showAlert, getConfirm } = useModal();
-
   const [categoryOpen, setCategoryOpen] = useState(false);
+  const location = useLocation();
+
+  const { logout, isLoggedIn } = useAuth();
+
   const [shouldRenderCategory, handleTransitionEnd, triggerAnimation] =
     useAnimation(categoryOpen);
 
-  const location = useLocation();
-
   const handleCategoryToggle = () => {
     setCategoryOpen((prevState) => !prevState);
-  };
-
-  const handleLogout = async () => {
-    const confirmation = await getConfirm('로그아웃 하시겠습니까?');
-    if (confirmation) {
-      try {
-        logout();
-        showAlert('로그아웃이 완료되었습니다.');
-      } catch {
-        showAlert('로그아웃에 실패했습니다. 다시 시도해주세요.');
-      }
-    }
   };
 
   useEffect(() => {
@@ -44,23 +30,23 @@ function HeaderNav() {
   return (
     <S.Nav>
       <S.Wrapper>
-        <S.FlexLeftUl>
+        <S.FlexLeftWrapper>
           <S.TransparentButton onClick={handleCategoryToggle} aria-label="카테고리">
             카테고리
           </S.TransparentButton>
           <Link to={ROUTES.PRODUCTS}>제품 검색</Link>
           <Link to={ROUTES.PROFILE_SEARCH}>프로필 검색</Link>
-        </S.FlexLeftUl>
-        <S.FlexRightUl>
+        </S.FlexLeftWrapper>
+        <S.FlexRightWrapper>
           {isLoggedIn ? (
             <>
               <S.ProfileLink to={ROUTES.PROFILE}>내 프로필</S.ProfileLink>
-              <S.TransparentButton onClick={handleLogout}>로그아웃</S.TransparentButton>
+              <S.TransparentButton onClick={logout}>로그아웃</S.TransparentButton>
             </>
           ) : (
-            <S.LoginButton href={GITHUB_AUTH_URL}>로그인</S.LoginButton>
+            <S.LoginLink href={GITHUB_AUTH_URL}>로그인</S.LoginLink>
           )}
-        </S.FlexRightUl>
+        </S.FlexRightWrapper>
       </S.Wrapper>
       {shouldRenderCategory && (
         <CategoryNav
--- frontend/src/hooks/useAuth.tsx
+++ frontend/src/hooks/useAuth.tsx
@@ -7,6 +7,7 @@ import {
 } from '@/contexts/LoginContextProvider';
 
 import useGet from '@/hooks/api/useGet';
+import useModal from '@/hooks/useModal';
 
 import { ENDPOINTS } from '@/constants/api';
 
@@ -18,8 +19,9 @@ type Return = {
 
 function useAuth(): Return {
   const setUserData = useContext(SetUserDataContext);
-  const removeUserData = useContext(LogoutContext);
+  const handleLogout = useContext(LogoutContext);
   const isLoggedIn = useContext(IsLoggedInContext);
+  const { showAlert, getConfirm } = useModal();
 
   const fetchUserData = useGet<UserData>({ url: ENDPOINTS.LOGIN });
 
@@ -28,8 +30,16 @@ function useAuth(): Return {
     setUserData(userData);
   };
 
-  const logout = () => {
-    removeUserData();
+  const logout = async () => {
+    const confirmation = await getConfirm('로그아웃 하시겠습니까?');
+    if (confirmation) {
+      try {
+        handleLogout();
+        showAlert('로그아웃이 완료되었습니다.');
+      } catch {
+        showAlert('로그아웃에 실패했습니다. 다시 시도해주세요.');
+      }
+    }
   };
 
   return { login, logout, isLoggedIn };
