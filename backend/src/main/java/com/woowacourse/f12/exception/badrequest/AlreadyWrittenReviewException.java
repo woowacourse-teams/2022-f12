501fd7676f5a685200238b24122bd82ad516d4da
yangdongjue5510 <djwhy5510@gmail.com>
2022-08-11 16:31:51 +0900

    refactor: 중복에 관련된 예외 코드 클래스 이름 변경
    Co-authored-by: Ohzzi <clover7even@naver.com>

--- backend/src/main/java/com/woowacourse/f12/exception/badrequest/AlreadyWrittenReviewException.java
+++ backend/src/main/java/com/woowacourse/f12/exception/badrequest/AlreadyWrittenReviewException.java
@@ -1,10 +1,10 @@
 package com.woowacourse.f12.exception.badrequest;
 
-import static com.woowacourse.f12.exception.ExceptionCode.DUPLICATED_CONTENT;
+import static com.woowacourse.f12.exception.ExceptionCode.DUPLICATED_DATA;
 
 public class AlreadyWrittenReviewException extends InvalidValueException {
 
     public AlreadyWrittenReviewException() {
-        super(DUPLICATED_CONTENT, "해당 제품에 대해 이미 리뷰가 작성되어 있습니다.");
+        super(DUPLICATED_DATA, "해당 제품에 대해 이미 리뷰가 작성되어 있습니다.");
     }
 }
