bf6a41fc97e5731f7be8cb8f971e631888c3ceb1
yoon <duddbs774@gmail.com>
2022-08-11 17:10:15 +0900

    feat: 회원과 제품으로 리뷰를 조회하는 API 구현
    Co-authored-by: hamcheeseburger <yoo77hyeon@gmail.com>

--- backend/src/test/java/com/woowacourse/f12/presentation/inventoryproduct/InventoryProductControllerTest.java
+++ backend/src/test/java/com/woowacourse/f12/presentation/inventoryproduct/InventoryProductControllerTest.java
@@ -1,10 +1,13 @@
 package com.woowacourse.f12.presentation.inventoryproduct;
 
 import static com.woowacourse.f12.support.InventoryProductFixtures.SELECTED_INVENTORY_PRODUCT;
+import static com.woowacourse.f12.support.MemberFixtures.CORINNE;
 import static com.woowacourse.f12.support.ProductFixture.KEYBOARD_1;
+import static com.woowacourse.f12.support.ReviewFixtures.REVIEW_RATING_1;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.BDDMockito.willThrow;
@@ -22,10 +25,10 @@ import com.woowacourse.f12.domain.inventoryproduct.InventoryProduct;
 import com.woowacourse.f12.domain.member.Member;
 import com.woowacourse.f12.dto.request.inventoryproduct.ProfileProductRequest;
 import com.woowacourse.f12.dto.response.inventoryproduct.InventoryProductsResponse;
+import com.woowacourse.f12.dto.response.review.ReviewResponse;
 import com.woowacourse.f12.exception.badrequest.DuplicatedProfileProductCategoryException;
 import com.woowacourse.f12.exception.badrequest.InvalidProfileProductCategoryException;
 import com.woowacourse.f12.exception.notfound.InventoryProductNotFoundException;
-import com.woowacourse.f12.support.MemberFixtures;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -199,7 +202,7 @@ class InventoryProductControllerTest {
     void 멤버_id_로_조회한다() throws Exception {
         // given
         Long memberId = 1L;
-        Member member = MemberFixtures.CORINNE.생성(memberId);
+        Member member = CORINNE.생성(memberId);
         InventoryProduct inventoryProduct = SELECTED_INVENTORY_PRODUCT.생성(1L, member, KEYBOARD_1.생성(1L));
         String authorizationHeader = "Bearer Token";
         given(jwtProvider.validateToken(authorizationHeader))
@@ -230,7 +233,7 @@ class InventoryProductControllerTest {
     void 다른_멤버_id_로_조회한다() throws Exception {
         // given
         Long memberId = 1L;
-        Member member = MemberFixtures.CORINNE.생성(memberId);
+        Member member = CORINNE.생성(memberId);
         InventoryProduct inventoryProduct = SELECTED_INVENTORY_PRODUCT.생성(1L, member, KEYBOARD_1.생성(1L));
         given(inventoryProductService.findByMemberId(memberId))
                 .willReturn(InventoryProductsResponse.from(List.of(inventoryProduct)));
@@ -246,5 +249,23 @@ class InventoryProductControllerTest {
         assertAll(
                 () -> verify(inventoryProductService).findByMemberId(memberId)
         );
+    }
+
+    @Test
+    void 인벤토리_아이디로_리뷰_조회_성공() throws Exception {
+        // given
+        Long inventoryId = 1L;
+        given(inventoryProductService.findReviewById(inventoryId))
+                .willReturn(ReviewResponse.from(REVIEW_RATING_1.작성(KEYBOARD_1.생성(), CORINNE.생성())));
+
+        // when
+        mockMvc.perform(
+                        get("/api/v1/inventoryProducts/" + inventoryId + "/reviews")
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        // then
+        verify(inventoryProductService).findReviewById(eq(inventoryId));
     }
 }
