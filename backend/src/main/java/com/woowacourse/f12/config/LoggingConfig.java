51c3f691fe3d6baa4a333e81960e6565640d7434
yangdongjue5510 <djwhy5510@gmail.com>
2022-08-16 15:07:41 +0900

    refactor: StopWatch 사용하도록 수정

--- backend/src/main/java/com/woowacourse/f12/config/LoggingConfig.java
+++ backend/src/main/java/com/woowacourse/f12/config/LoggingConfig.java
@@ -1,20 +1,16 @@
 package com.woowacourse.f12.config;
 
-import com.woowacourse.f12.support.RequestLogTimer;
-import java.time.Clock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StopWatch;
+import org.springframework.web.context.annotation.RequestScope;
 
 @Configuration
 public class LoggingConfig {
 
     @Bean
-    public Clock clock() {
-        return Clock.systemDefaultZone();
-    }
-
-    @Bean
-    public RequestLogTimer requestLogTimer() {
-        return new RequestLogTimer(clock());
+    @RequestScope
+    public StopWatch stopWatch() {
+        return new StopWatch();
     }
 }
--- backend/src/main/java/com/woowacourse/f12/presentation/LoggingInterceptor.java
+++ backend/src/main/java/com/woowacourse/f12/presentation/LoggingInterceptor.java
@@ -1,10 +1,10 @@
 package com.woowacourse.f12.presentation;
 
-import com.woowacourse.f12.support.RequestLogTimer;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
@@ -16,15 +16,15 @@ public class LoggingInterceptor implements HandlerInterceptor {
     private static final String REQUEST_LOG_FORMAT = "METHOD: {}, URL : {}, AUTHORIZATION : {}, BODY : {}";
     private static final String RESPONSE_LOG_FORMAT = "STATUS_CODE: {}, URL : {}, BODY : {}, TIME_TAKEN : {}";
 
-    private final RequestLogTimer requestLogTimer;
+    private final StopWatch requestLogTimer;
 
-    public LoggingInterceptor(final RequestLogTimer requestLogTimer) {
+    public LoggingInterceptor(final StopWatch requestLogTimer) {
         this.requestLogTimer = requestLogTimer;
     }
 
     @Override
-    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler)
-            throws Exception {
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response,
+                             final Object handler) {
         requestLogTimer.start();
 
         final String body = new String(new ContentCachingRequestWrapper(request).getContentAsByteArray());
@@ -35,13 +35,12 @@ public class LoggingInterceptor implements HandlerInterceptor {
 
     @Override
     public void afterCompletion(final HttpServletRequest request, final HttpServletResponse response,
-                                final Object handler, final Exception ex)
-            throws Exception {
+                                final Object handler, final Exception ex) {
         requestLogTimer.stop();
 
         final ContentCachingResponseWrapper contentCachingResponseWrapper = new ContentCachingResponseWrapper(response);
         final String responseBody = new String(contentCachingResponseWrapper.getContentAsByteArray());
         log.info(RESPONSE_LOG_FORMAT, response.getStatus(), request.getRequestURI(), responseBody,
-                requestLogTimer.getTakenTime());
+                requestLogTimer.getLastTaskTimeMillis());
     }
 }
