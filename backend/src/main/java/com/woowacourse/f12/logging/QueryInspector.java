95d5d4bd8d593523086c37e97a5b953f94030828
Ohzzi <clover7even@naver.com>
2022-08-16 11:38:16 +0900

    fix: QueryInspector의 로그 제거

--- backend/src/main/java/com/woowacourse/f12/logging/QueryInspector.java
+++ backend/src/main/java/com/woowacourse/f12/logging/QueryInspector.java
@@ -1,15 +1,11 @@
 package com.woowacourse.f12.logging;
 
-import lombok.extern.slf4j.Slf4j;
 import org.hibernate.resource.jdbc.spi.StatementInspector;
 import org.springframework.beans.factory.support.ScopeNotActiveException;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 public class QueryInspector implements StatementInspector {
-
-    private static final String QUERY_LOG_FORMAT = "QUERY: {}";
 
     private final QueryCounter queryCounter;
 
@@ -21,7 +17,6 @@ public class QueryInspector implements StatementInspector {
     public String inspect(final String sql) {
         try {
             queryCounter.increaseCount();
-            log.info(QUERY_LOG_FORMAT, sql);
         } catch (ScopeNotActiveException ignored) {
         }
         return sql;
