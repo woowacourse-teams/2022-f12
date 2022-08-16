e093a86523b3a3dc9cf13952ffccddb2cf235e0b
Ohzzi <clover7even@naver.com>
2022-08-16 16:26:06 +0900

    test: request scope 테스트 오류 수정

--- backend/src/test/java/com/woowacourse/f12/logging/ApiQueryInspectorTest.java
+++ backend/src/test/java/com/woowacourse/f12/logging/ApiQueryInspectorTest.java
@@ -2,16 +2,28 @@ package com.woowacourse.f12.logging;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+@SpringBootTest
 class ApiQueryInspectorTest {
+
+    @Autowired
+    private ApiQueryInspector apiQueryInspector;
+
+    @BeforeEach
+    void setUp() {
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(new MockHttpServletRequest()));
+    }
 
     @Test
     void inspect는_실행될_sql문을_그대로_반환한다() {
-        // given
-        ApiQueryInspector apiQueryInspector = new ApiQueryInspector(new QueryCounter());
-
-        // when
+        // given, when
         String inspectResult = apiQueryInspector.inspect("sql");
 
         // then
@@ -20,10 +32,7 @@ class ApiQueryInspectorTest {
 
     @Test
     void inspect가_실행되면_QueryCounter의_count를_증가시킨다() {
-        // given
-        ApiQueryInspector apiQueryInspector = new ApiQueryInspector(new QueryCounter());
-
-        // when
+        // given, when
         apiQueryInspector.inspect("sql");
 
         // then
