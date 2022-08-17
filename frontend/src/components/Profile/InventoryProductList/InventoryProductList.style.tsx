da38baf6c9a0514c0cff886750ed55c8325751f2
jswith <typingjs@gmail.com>
2022-08-17 15:28:39 +0900

    style: grid 세로 gap 조정

--- frontend/src/components/Profile/InventoryProductList/InventoryProductList.style.tsx
+++ frontend/src/components/Profile/InventoryProductList/InventoryProductList.style.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(6, 1fr);
+  grid-gap: 1rem;
 `;
 
 export const CategoryTitle = styled.h2`
