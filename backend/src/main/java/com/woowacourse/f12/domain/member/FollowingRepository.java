058cea1d99d5d46e0ebb5c645b688e1586b8e3c3
corinne <yoo77hyeon@gmail.com>
2022-08-17 16:58:53 +0900

    feat: 팔로워 id와 팔로이 id로 팔로잉 객체를 조회하는 기능 추가
    Co-authored-by: Ohzzi <clover7even@naver.com>

--- backend/src/main/java/com/woowacourse/f12/domain/member/FollowingRepository.java
+++ backend/src/main/java/com/woowacourse/f12/domain/member/FollowingRepository.java
@@ -2,7 +2,11 @@ package com.woowacourse.f12.domain.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FollowingRepository extends JpaRepository<Following, Long> {
 
     boolean existsByFollowerIdAndFolloweeId(Long followerId, Long followeeId);
+
+    Optional<Following> findByFollowerIdAndFolloweeId(Long followerId, Long followeeId);
 }
