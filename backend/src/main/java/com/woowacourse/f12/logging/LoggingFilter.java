2812610b7065cd7dcac0c27e236f5a77340e4507
Youngyoon-1 <76875654+Youngyoon-1@users.noreply.github.com>
2022-08-17 20:43:26 +0900

    [BugFix/BE] 로그에 QueryParam이 출력되지 않는 문제 해결 (#498) (#499)
    * [BugFix/BE] 로그에 QueryParam이 출력되지 않는 문제 해결 (#498)
    
    fix: uri에 query parameter도 출력되도록 수정, body가 json 값이 아닌 경우이거나 없는 경우 출력하지 않도록 수정
    
    Co-authored-by: yangdongjue5510 <djwhy5510@gmail.com>
    
    * refactor: 리뷰 반영
    
    Co-authored-by: yangdongjue5510 <djwhy5510@gmail.com>

--- backend/src/main/java/com/woowacourse/f12/logging/LoggingFilter.java
+++ backend/src/main/java/com/woowacourse/f12/logging/LoggingFilter.java
@@ -1,11 +1,18 @@
 package com.woowacourse.f12.logging;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,12 +23,17 @@ import org.springframework.web.util.ContentCachingResponseWrapper;
 @Component
 public class LoggingFilter extends OncePerRequestFilter {
 
-    private static final String REQUEST_LOG_FORMAT = "METHOD: {}, URL: {}, AUTHORIZATION: {}, BODY: {}";
-    private static final String RESPONSE_LOG_FORMAT =
-            "STATUS_CODE: {}, METHOD: {}, URL: {}, QUERY_COUNT: {}, TIME_TAKEN: {}ms, BODY: {}";
+    private static final String REQUEST_LOG_NO_BODY_FORMAT = "REQUEST :: METHOD: {}, URL: {}, AUTHORIZATION: {}";
+    private static final String REQUEST_LOG_FORMAT = REQUEST_LOG_NO_BODY_FORMAT + ", BODY: {}";
+    private static final String RESPONSE_LOG_NO_BODY_FORMAT = "RESPONSE :: STATUS_CODE: {}, METHOD: {}, URL: {}, QUERY_COUNT: {}, TIME_TAKEN: {}ms";
+    private static final String RESPONSE_LOG_FORMAT = RESPONSE_LOG_NO_BODY_FORMAT + ", BODY: {}";
     private static final String QUERY_COUNT_WARNING_LOG_FORMAT = "쿼리가 {}번 이상 실행되었습니다.";
 
     private static final int QUERY_COUNT_WARNING_STANDARD = 10;
+    private static final String START_OF_PARAMS = "?";
+    private static final String PARAM_DELIMITER = "&";
+    private static final String KEY_VALUE_DELIMITER = "=";
+    private static final String AUTHORIZATION = "Authorization";
 
     private final StopWatch apiTimer;
     private final ApiQueryCounter apiQueryCounter;
@@ -48,15 +60,68 @@ public class LoggingFilter extends OncePerRequestFilter {
 
     private void logRequestAndResponse(final ContentCachingRequestWrapper request,
                                        final ContentCachingResponseWrapper response) {
-        final int queryCount = apiQueryCounter.getCount();
-        final String requestBody = new String(request.getContentAsByteArray());
-        final String responseBody = new String(response.getContentAsByteArray());
-
-        log.info(REQUEST_LOG_FORMAT, request.getMethod(), request.getRequestURI(),
-                request.getHeader("Authorization"), requestBody);
-        log.info(RESPONSE_LOG_FORMAT, response.getStatus(), request.getMethod(), request.getRequestURI(),
-                queryCount, apiTimer.getLastTaskTimeMillis(), responseBody);
+        logRequest(request);
+        logResponse(request, response);
         warnByQueryCount();
+    }
+
+    private void logRequest(final ContentCachingRequestWrapper request) {
+        final String requestBody = new String(request.getContentAsByteArray());
+        final String requestURIWithParams = getRequestURIWithParams(request);
+
+        if (requestBody.isBlank()) {
+            log.info(REQUEST_LOG_NO_BODY_FORMAT, request.getMethod(), requestURIWithParams,
+                    request.getHeader(AUTHORIZATION));
+            return;
+        }
+        log.info(REQUEST_LOG_FORMAT, request.getMethod(), requestURIWithParams,
+                request.getHeader(AUTHORIZATION), requestBody);
+    }
+
+    private void logResponse(final ContentCachingRequestWrapper request, final ContentCachingResponseWrapper response) {
+        final Optional<String> jsonResponseBody = getJsonResponseBody(response);
+        final int queryCount = apiQueryCounter.getCount();
+        final String requestURIWithParams = getRequestURIWithParams(request);
+
+        if (jsonResponseBody.isEmpty()) {
+            log.info(RESPONSE_LOG_NO_BODY_FORMAT, response.getStatus(), request.getMethod(),
+                    requestURIWithParams, queryCount, apiTimer.getLastTaskTimeMillis());
+            return;
+        }
+        log.info(RESPONSE_LOG_FORMAT, response.getStatus(), request.getMethod(), requestURIWithParams,
+                queryCount, apiTimer.getLastTaskTimeMillis(), jsonResponseBody.get());
+    }
+
+    private String getRequestURIWithParams(final ContentCachingRequestWrapper request) {
+        final String requestURI = request.getRequestURI();
+        final Map<String, String[]> params = request.getParameterMap();
+        if (params.isEmpty()) {
+            return requestURI;
+        }
+        final String parsedParams = parseParams(params);
+        return requestURI + parsedParams;
+    }
+
+    private String parseParams(final Map<String, String[]> params) {
+        final String everyParamStrings = params.entrySet().stream()
+                .map(this::toParamString)
+                .collect(Collectors.joining(PARAM_DELIMITER));
+        return START_OF_PARAMS + everyParamStrings;
+    }
+
+    private String toParamString(final Entry<String, String[]> entry) {
+        final String key = entry.getKey();
+        final StringBuilder builder = new StringBuilder();
+        return Arrays.stream(entry.getValue())
+                .map(value -> builder.append(key).append(KEY_VALUE_DELIMITER).append(value))
+                .collect(Collectors.joining(PARAM_DELIMITER));
+    }
+
+    private Optional<String> getJsonResponseBody(final ContentCachingResponseWrapper response) {
+        if (Objects.equals(response.getContentType(), MediaType.APPLICATION_JSON_VALUE)) {
+            return Optional.of(new String(response.getContentAsByteArray()));
+        }
+        return Optional.empty();
     }
 
     private void warnByQueryCount() {
