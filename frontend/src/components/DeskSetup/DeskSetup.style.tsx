2c9b29a5967b112f5b23ef3066bf21645ceeec6c
Youngmin Hong <typingjs@gmail.com>
2022-08-17 14:07:18 +0900

    [FE] 프로필 페이지의 UI를 개선 (#493)
    * style: 기본 UI 구현
    
    * feat: 프로필 페이지 개선 완료
    
    * refactor: 사용하지 않는 import문을 제거
    
    * refactor: 불필요한 코드 삭제 및 리팩토링
    
    * style: 팔로우 버튼 위치 조정

---
+++ frontend/src/components/DeskSetup/DeskSetup.style.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+export const CardWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 4rem;
+`;
--- frontend/src/pages/Profile/Profile.style.tsx
+++ frontend/src/pages/Profile/Profile.style.tsx
@@ -2,24 +2,30 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  justify-content: space-around;
+  flex-direction: column;
+  align-items: center;
+  gap: 3rem;
 `;
 
 export const ProfileSection = styled.section`
-  width: 45%;
-
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
-  gap: 1.5rem;
+  width: 100%;
+  gap: 1rem;
 `;
 
 export const InventorySection = styled.section`
-  width: 45%;
-
+  width: 95%;
   display: flex;
   flex-direction: column;
-  gap: 1.5rem;
+  gap: 2rem;
+`;
+
+export const InventoryItemHeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
 
 export const Title = styled.h1`
@@ -34,3 +40,24 @@ export const InventoryProductList = styled.div`
 `;
 
 export const Description = styled.p``;
+
+export const FollowButton = styled.button`
+  /* align-self: end; */
+  padding: 0.4rem 1.4rem;
+  background-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 0.4rem;
+  box-shadow: 4px 4px 10px ${({ theme }) => theme.colors.secondary};
+`;
+
+export const EditDeskSetupButton = styled.button`
+  padding: 0.4rem 1.4rem;
+  background-color: ${({ theme }) => theme.colors.primary};
+  border-radius: 0.4rem;
+  box-shadow: 4px 4px 10px ${({ theme }) => theme.colors.secondary};
+`;
+
+export const DeskSetupSection = styled.section`
+  width: 100%;
+  height: 25rem;
+  background-color: ${({ theme }) => theme.colors.secondary};
+`;
