f531ba4655f105726dad6de47e01ac6e6a836e18
Yo Wook Kim <uk960214@gmail.com>
2022-08-17 15:17:58 +0900

    [Feature/FE] 루트를 새로 렌더링 할 때 페이지 최상단으로 스크롤하도록 수정 (#504)
    fix: 루트를 새로 렌더링 할 때 페이지 최상단으로 스크롤하도록 수정

--- frontend/src/pages/common/PageLayout/PageLayout.tsx
+++ frontend/src/pages/common/PageLayout/PageLayout.tsx
@@ -1,11 +1,21 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import * as S from '@/pages/common/PageLayout/PageLayout.style';
 
 import HeaderLogo from '@/components/common/HeaderLogo/HeaderLogo';
 import HeaderNav from '@/components/common/HeaderNav/HeaderNav';
+import { useEffect } from 'react';
 
 function PageLayout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+    });
+  }, [location.key]);
+
   return (
     <>
       <HeaderLogo />
