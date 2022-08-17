c5b8a050b76da4c8129473404923d9b1d109d957
corinne <yoo77hyeon@gmail.com>
2022-08-17 14:36:43 +0900

    feat: Following 도메인 생성
    Co-authored-by: Ohzzi <clover7even@naver.com>

---
+++ backend/src/main/java/com/woowacourse/f12/domain/member/FollowingRepository.java
@@ -0,0 +1,6 @@
+package com.woowacourse.f12.domain.member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FollowingRepository extends JpaRepository<Following, Long> {
+}
