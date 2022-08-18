15c37cea7abd935d65cb729bbea51359a076fe8f
corinne <yoo77hyeon@gmail.com>
2022-08-18 11:28:18 +0900

    feat: 팔로잉하는 사용자 목록을 키워드와 옵션으로 검색하는 Repository 기능 구현
    Co-authored-by: Ohzzi <clover7even@naver.com>

--- backend/src/test/java/com/woowacourse/f12/domain/member/MemberRepositoryTest.java
+++ backend/src/test/java/com/woowacourse/f12/domain/member/MemberRepositoryTest.java
@@ -10,13 +10,16 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
+import static com.woowacourse.f12.domain.member.CareerLevel.JUNIOR;
 import static com.woowacourse.f12.domain.member.CareerLevel.SENIOR;
 import static com.woowacourse.f12.domain.member.JobType.BACKEND;
+import static com.woowacourse.f12.domain.member.JobType.FRONTEND;
 import static com.woowacourse.f12.support.MemberFixtures.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -137,6 +140,41 @@ class MemberRepositoryTest {
                 () -> assertThat(slice.hasNext()).isFalse(),
                 () -> assertThat(slice.getContent()).usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
                         .containsOnly(CORINNE.생성())
+        );
+    }
+
+    @Test
+    void 팔로잉하는_회원의_목록을_키워드와_옵션으로_조회한다() {
+        // given
+        Member corinne = CORINNE.생성();
+        Member mincho = MINCHO.생성();
+        Member ohzzi = OHZZI.생성();
+        memberRepository.saveAll(List.of(corinne, mincho, ohzzi));
+        followingRepository.save(Following.builder()
+                .followerId(corinne.getId())
+                .followeeId(mincho.getId())
+                .build());
+        entityManager.clear();
+
+        Member expected = Member.builder()
+                .id(mincho.getId())
+                .gitHubId(mincho.getGitHubId())
+                .name(mincho.getName())
+                .imageUrl(mincho.getImageUrl())
+                .careerLevel(mincho.getCareerLevel())
+                .jobType(mincho.getJobType())
+                .followerCount(1)
+                .build();
+
+        // when
+        Slice<Member> slice = memberRepository.findFolloweesBySearchConditions(corinne.getId(), "jswith", JUNIOR, FRONTEND,
+                PageRequest.of(0, 1, Sort.by("id").descending()));
+
+        // then
+        assertAll(
+                () -> assertThat(slice.hasNext()).isFalse(),
+                () -> assertThat(slice.getContent()).usingRecursiveFieldByFieldElementComparator()
+                        .containsExactly(expected)
         );
     }
 
