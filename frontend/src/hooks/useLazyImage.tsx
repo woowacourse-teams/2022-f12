9f26a0de02d540644a88135a430ed2df596d4781
Youngmin Hong <typingjs@gmail.com>
2022-09-12 21:11:06 +0900

    [FE] 이미지 lazy loading 구현 (#565)
    * feat: useLazyImage
    
    * feat: LazyImage 컴포넌트
    
    * feat: lazyLoading 적용

---
+++ frontend/src/hooks/useLazyImage.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useRef, useState } from 'react';
+
+type Props = {
+  src: string;
+};
+
+function useLazyImage({ src }: Props) {
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    let imageContentObserver;
+
+    if (imageRef && !imageSrc) {
+      imageContentObserver = new IntersectionObserver(
+        ([entry]) => {
+          if (entry.isIntersecting) {
+            setImageSrc(src);
+            imageContentObserver.unobserve(imageRef.current);
+          }
+        },
+        { threshold: [0.5] }
+      );
+
+      imageContentObserver.observe(imageRef.current);
+    }
+
+    return () => {
+      imageContentObserver && imageContentObserver.disconnect(imageRef);
+    };
+  }, [imageRef, imageSrc, src]);
+
+  return { imageRef, imageSrc };
+}
+
+export default useLazyImage;
