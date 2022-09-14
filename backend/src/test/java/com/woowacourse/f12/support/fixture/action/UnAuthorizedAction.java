55535c5f81c3999a9c0655ed62b83e4ea7bfe19a
Ohzzi <clover7even@naver.com>
2022-09-14 20:25:09 +0900

    test: 생성 메서드가 먼저 나오도록 메서드 순서 조정

--- backend/src/test/java/com/woowacourse/f12/support/fixture/action/UnAuthorizedAction.java
+++ backend/src/test/java/com/woowacourse/f12/support/fixture/action/UnAuthorizedAction.java
@@ -10,11 +10,11 @@ public class UnAuthorizedAction {
     private UnAuthorizedAction() {
     }
 
-    public ExtractableResponse<Response> 리뷰_목록_페이지를_조회한다(final int page, final int size, final String sortCondition) {
-        return GET_요청을_보낸다(String.format("/api/v1/reviews?page=%d&size=%d&sort=%s,desc", page, size, sortCondition));
-    }
-
     public static UnAuthorizedAction 로그인하지_않고() {
         return new UnAuthorizedAction();
+    }
+
+    public ExtractableResponse<Response> 리뷰_목록_페이지를_조회한다(final int page, final int size, final String sortCondition) {
+        return GET_요청을_보낸다(String.format("/api/v1/reviews?page=%d&size=%d&sort=%s,desc", page, size, sortCondition));
     }
 }
