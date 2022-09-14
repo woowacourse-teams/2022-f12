0a4b2040fc992524636f9052555d287c4a944a75
Dongjue Yang <87690744+yangdongjue5510@users.noreply.github.com>
2022-09-14 15:13:54 +0900

    [Feature/BE] 1000자 이상인 URI로 요청하는 경우 예외 반환 (#571)
    * [BE] 1000자 이상인 URI로 요청하는 경우 예외 반환 (#570)
    
    feat: interceptor를 활용해서 1000자 이상인 URI로 요청하는 경우 예외 발생
    
    * [BE] URI 길이 제한 기능 코드리뷰 반영 (#580)
    
    * refactor: InvalidUrlLengthException 최대 길이 주입받도록 수정
    
    * test: 테스트하는 URI 길이가 1000자가 되도록 수정
    
    * fix: URI가 1000자를 넘기는 경우 상태 코드 414 를 반환

---
+++ backend/src/test/java/com/woowacourse/f12/presentation/UrlLengthCheckInterceptorTest.java
@@ -0,0 +1,45 @@
+package com.woowacourse.f12.presentation;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.woowacourse.f12.application.product.ProductService;
+import com.woowacourse.f12.exception.ErrorCode;
+import com.woowacourse.f12.presentation.product.ProductController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(ProductController.class)
+class UrlLengthCheckInterceptorTest extends PresentationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
+
+    @Test
+    void uri가_1000자_이상인_경우_예외가_발생() throws Exception {
+        // given
+        final String uriPrefix = "/api/v1/products?page=0&query=";
+        final String uriSuffix = "&sort=reviewCount,desc&size=12";
+        final String tooLongQuery = "1".repeat(1000 - uriPrefix.length() - uriSuffix.length());
+
+        // when, then
+        mockMvc.perform(
+                        get(uriPrefix + tooLongQuery + uriSuffix))
+                .andExpect(status().isUriTooLong())
+                .andExpect(jsonPath("$.errorCode").value(ErrorCode.URI_TOO_LONG.getValue()))
+                .andDo(print());
+
+        // then
+        verify(productService, times(0)).findBySearchConditions(any(), any());
+    }
+}
