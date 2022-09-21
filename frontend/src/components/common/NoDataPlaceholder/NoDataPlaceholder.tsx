f22a4cf3230474dd03eb3d1365f84452e03cd191
Yo Wook Kim <uk960214@gmail.com>
2022-09-21 14:23:55 +0900

    [FE] SVG 오류 수정, 오류 이미지 위치 수정 (#630)
    * fix: svg 이미지에서 너비 및 높이 속성 삭제
    
    * style: 검색 결과 없음 이미지 크기 및 위치 조정
    
    * fix: 오류 이미지 위치 조정

--- frontend/src/components/common/NoDataPlaceholder/NoDataPlaceholder.tsx
+++ frontend/src/components/common/NoDataPlaceholder/NoDataPlaceholder.tsx
@@ -2,10 +2,10 @@ import Empty from '@/assets/empty.svg';
 
 function NoDataPlaceholder() {
   return (
-    <>
+    <div>
       <Empty />
       <div style={{ width: '100%', textAlign: 'center' }}>아무것도 찾지 못했어요..</div>
-    </>
+    </div>
   );
 }
 
