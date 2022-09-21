84ca31c79c05e9a1265f81a8400f1882f7ecf5e6
Dongjue Yang <87690744+yangdongjue5510@users.noreply.github.com>
2022-09-21 14:58:07 +0900

    [Feature/BE] Review 페이징 조회 시 2N+1 문제 fetch join으로 해결 (#608)
    refactor: Review 페이징 조회 시 2N+1 문제 fetch join으로 해결

--- backend/src/main/java/com/woowacourse/f12/domain/review/ReviewRepository.java
+++ backend/src/main/java/com/woowacourse/f12/domain/review/ReviewRepository.java
@@ -10,14 +10,16 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewRepositoryCustom {
 
-    @Query("select r from Review r where r.product.id = :productId")
+    @Query("select r from Review r join fetch r.member join fetch r.product where r.product.id = :productId")
     Slice<Review> findPageByProductId(Long productId, Pageable pageable);
 
+    @Query("select r from Review r join fetch r.member join fetch r.product")
     Slice<Review> findPageBy(Pageable pageable);
 
     boolean existsByMemberAndProduct(Member member, Product product);
 
     Optional<Review> findByMemberAndProduct(Member member, Product product);
 
+    @Query("select r from Review r join fetch r.member join fetch r.product where r.member.id = :memberId")
     Slice<Review> findPageByMemberId(Long memberId, Pageable pageable);
 }
