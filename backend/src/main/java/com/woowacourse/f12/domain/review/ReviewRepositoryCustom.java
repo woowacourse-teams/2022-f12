e3576466d9e74fc09262893db31c9c1a237510c2
Dongjue Yang <87690744+yangdongjue5510@users.noreply.github.com>
2022-09-22 16:30:19 +0900

    [Feature/BE] 리뷰 조회 시 커버링 인덱스를 사용한다 (#666)
    [BE] 리뷰 조회 시 커버링 인덱스를 사용한다. (#665)
    
    * feat: security 최신화
    
    * refactor: 리뷰 전체 조회 쿼리 성능 개선

--- backend/src/main/java/com/woowacourse/f12/domain/review/ReviewRepositoryCustom.java
+++ backend/src/main/java/com/woowacourse/f12/domain/review/ReviewRepositoryCustom.java
@@ -1,8 +1,12 @@
 package com.woowacourse.f12.domain.review;
 
 import java.util.List;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface ReviewRepositoryCustom {
+
+    Slice<Review> findPageBy(Pageable pageable);
 
     List<CareerLevelCount> findCareerLevelCountByProductId(Long productId);
 
