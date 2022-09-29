7761c61b052377dca00dfa46f5388e49adb967e4
Yo Wook Kim <uk960214@gmail.com>
2022-09-29 20:34:15 +0900

    refactor: 프로필 카드 데스크 셋업 제품 리팩토링

--- frontend/src/components/Profile/ProfileCard/ProfileCard.tsx
+++ frontend/src/components/Profile/ProfileCard/ProfileCard.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import Chip from '@/components/common/Chip/Chip';
@@ -10,6 +10,7 @@ import useFollowing from '@/hooks/useFollowing';
 
 import TITLE from '@/constants/header';
 import { GITHUB_IMAGE_SIZE_SEARCH_PARAM, GITHUB_URL } from '@/constants/link';
+import { CATEGORIES } from '@/constants/product';
 import { CAREER_LEVELS, JOB_TYPES } from '@/constants/profile';
 import ROUTES from '@/constants/routes';
 
@@ -28,6 +29,16 @@ type ProfileProduct = {
   rating: number;
   category: string;
 };
+
+type DeskSetupProduct = Pick<Product, 'id' | 'name' | 'imageUrl'>;
+type ProfileProductCategories = Exclude<Category, 'software'>;
+
+const profileCategories: ProfileProductCategories[] = [
+  'keyboard',
+  'mouse',
+  'monitor',
+  'stand',
+];
 
 type Props = {
   id: number;
@@ -92,31 +103,52 @@ function ProfileCard({
     }
   };
 
-  const keyboard = profileProducts.find((product) => product.category === 'keyboard') || {
-    id: null,
-    imageUrl: null,
-    name: '키보드',
-  };
-  const mouse = profileProducts.find((product) => product.category === 'mouse') || {
-    id: null,
-    imageUrl: null,
-    name: '마우스',
-  };
-  const monitor = profileProducts.find((product) => product.category === 'monitor') || {
-    id: null,
-    imageUrl: null,
-    name: '모니터',
-  };
-  const stand = profileProducts.find((product) => product.category === 'stand') || {
-    id: null,
-    imageUrl: null,
-    name: '거치대',
-  };
-  const representativeEquipments = [keyboard, mouse, monitor, stand].sort(
-    (prevEquipment, nextEquipment) => {
-      return nextEquipment?.name.length - prevEquipment?.name.length;
-    }
+  const getProductByCategory = (categoryName: ProfileProductCategories) =>
+    profileProducts.find((product) => product.category === categoryName);
+
+  const deskSetupProduct = ({ id, name, imageUrl }: DeskSetupProduct) => (
+    <S.InventoryItem key={id}>
+      <S.ProductImageWrapper>
+        <Link to={`${ROUTES.PRODUCT}/${id}`}>
+          <S.ProductImage src={imageUrl} />
+        </Link>
+        <S.ProductTitle>
+          <Link to={`${ROUTES.PRODUCT}/${id}`}>{name}</Link>
+        </S.ProductTitle>
+      </S.ProductImageWrapper>
+    </S.InventoryItem>
   );
+
+  const emptyDeskSetupProduct = (categoryName: ProfileProductCategories) => (
+    <S.InventoryItem key={categoryName}>
+      <S.ProductImageWrapper>
+        <Empty />
+        <S.ProductTitle>
+          {TITLE.DESK_SETUP}에 추가한 {CATEGORIES[categoryName]}가 없어요
+        </S.ProductTitle>
+      </S.ProductImageWrapper>
+    </S.InventoryItem>
+  );
+
+  const deskSetupProducts = useMemo(() => {
+    const result: JSX.Element[] = [];
+    const noProductQueue: ProfileProductCategories[] = [];
+
+    profileCategories.forEach((category) => {
+      const product = getProductByCategory(category);
+      if (product === undefined) {
+        noProductQueue.push(category);
+      } else {
+        result.push(deskSetupProduct(product));
+      }
+    });
+
+    result.push(
+      ...noProductQueue.map((categoryName) => emptyDeskSetupProduct(categoryName))
+    );
+
+    return result;
+  }, []);
 
   return (
     <S.Container index={index}>
@@ -155,32 +187,7 @@ function ProfileCard({
             <PrevSign />
           </S.LeftButton>
           <S.InventoryListWrapper>
-            <S.InventoryList positionX={positionX}>
-              {representativeEquipments.map((equipment, index) => {
-                return (
-                  <S.InventoryItem key={index}>
-                    <S.ProductImageWrapper>
-                      {equipment.id ? (
-                        <Link to={`${ROUTES.PRODUCT}/${equipment.id as string}`}>
-                          <S.ProductImage src={equipment.imageUrl as string} />
-                        </Link>
-                      ) : (
-                        <Empty />
-                      )}
-                      <S.ProductTitle>
-                        {equipment.id ? (
-                          <Link to={`${ROUTES.PRODUCT}/${equipment.id as string}`}>
-                            {equipment.name}
-                          </Link>
-                        ) : (
-                          `${TITLE.DESK_SETUP}에 추가한 ${equipment.name}가 없어요`
-                        )}
-                      </S.ProductTitle>
-                    </S.ProductImageWrapper>
-                  </S.InventoryItem>
-                );
-              })}
-            </S.InventoryList>
+            <S.InventoryList positionX={positionX}>{deskSetupProducts}</S.InventoryList>
           </S.InventoryListWrapper>
           <S.RightButton onClick={handleRightButtonClick}>
             <NextSign />
