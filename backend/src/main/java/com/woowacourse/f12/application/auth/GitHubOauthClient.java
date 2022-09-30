77d6d202297b7df50ad64e0529ba0090f44743ec
Dongjue Yang <87690744+yangdongjue5510@users.noreply.github.com>
2022-09-30 15:39:40 +0900

    [Feature/BE] WebClient를 HttpClient로 변경 (#684)
    * [BE] 스프린트5에 대한 버퍼를 진행한다 (#678)
    
    * [BE] 스프린트5에 대한 버퍼를 진행한다 (#677)
    
    * refactor: 팔로잉된 회원의 아이디를 목록으로 가져올 때 조인하지 않도록 변경
    
    * refactor: 회원을 조회할 때 검색된 결과가 없다면, 다음 로직이 일어나지 않도록 변경
    
    * refactor: findByContains 메서드 명 변경
    
    * test: MemberServiceTest 한글 메서드 명 변경
    
    * feat: WebClient를 HttpClient로 교체 (#683)
    
    * feat: WebClient를 HttpClient로 교체
    
    * feat: WebClient를 HttpClient로 변경
    
    * chore: webflux 의존성 제거
    
    * refactor: InterruptedException 예외처리 구현
    
    * refactor: InterruptedException 예외 처리 구현
    
    * docs: rest docs 문서 수정
    
    * refactor: 코드 리뷰 반영
    
    * refactor: 디미터의 법칙 적용
    
    Co-authored-by: tiki <78459713+yh20studio@users.noreply.github.com>

--- backend/src/main/java/com/woowacourse/f12/application/auth/GitHubOauthClient.java
+++ backend/src/main/java/com/woowacourse/f12/application/auth/GitHubOauthClient.java
@@ -1,17 +1,28 @@
 package com.woowacourse.f12.application.auth;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.woowacourse.f12.dto.request.auth.GitHubTokenRequest;
 import com.woowacourse.f12.dto.response.auth.GitHubProfileResponse;
 import com.woowacourse.f12.dto.response.auth.GitHubTokenResponse;
 import com.woowacourse.f12.exception.badrequest.InvalidGitHubLoginException;
 import com.woowacourse.f12.exception.internalserver.GitHubServerException;
+import com.woowacourse.f12.exception.internalserver.OauthProcessingException;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpClient.Redirect;
+import java.net.http.HttpClient.Version;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.time.Duration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
 @Component
 public class GitHubOauthClient {
@@ -20,6 +31,13 @@ public class GitHubOauthClient {
     private final String secret;
     private final String accessTokenUrl;
     private final String profileUrl;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final HttpClient httpClient = HttpClient.newBuilder()
+            .version(Version.HTTP_2)
+            .followRedirects(Redirect.NORMAL)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
 
     public GitHubOauthClient(@Value("${github.client.id}") final String clientId,
                              @Value("${github.client.secret}") final String secret,
@@ -33,49 +51,93 @@ public class GitHubOauthClient {
 
     public String getAccessToken(final String code) {
         final GitHubTokenRequest gitHubTokenRequest = new GitHubTokenRequest(clientId, secret, code);
-        final WebClient webClient = WebClient.builder()
-                .baseUrl(accessTokenUrl)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-        return requestAccessToken(gitHubTokenRequest, webClient).getAccessToken();
+        final String requestBody = toRequestBody(gitHubTokenRequest);
+        try {
+            final HttpResponse<String> accessTokenResponse = requestAccessToken(requestBody);
+            return parseAccessToken(accessTokenResponse);
+        } catch (IOException e) {
+            throw new OauthProcessingException();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new OauthProcessingException();
+        }
     }
 
-    private GitHubTokenResponse requestAccessToken(final GitHubTokenRequest gitHubTokenRequest,
-                                                   final WebClient webClient) {
-        return webClient.post()
-                .accept(MediaType.APPLICATION_JSON)
-                .body(Mono.just(gitHubTokenRequest), GitHubTokenRequest.class)
-                .retrieve()
-                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
-                    throw new InvalidGitHubLoginException();
-                })
-                .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
-                    throw new GitHubServerException();
-                })
-                .bodyToMono(GitHubTokenResponse.class)
-                .blockOptional()
-                .orElseThrow(InvalidGitHubLoginException::new);
+    private String toRequestBody(final GitHubTokenRequest gitHubTokenRequest) {
+        try {
+            return objectMapper.writeValueAsString(gitHubTokenRequest);
+        } catch (JsonProcessingException e) {
+            throw new OauthProcessingException();
+        }
+    }
+
+    private HttpResponse<String> requestAccessToken(final String requestBody) throws IOException, InterruptedException {
+        final HttpRequest accessTokenRequest = buildAccessTokenRequest(requestBody);
+        return httpClient.send(accessTokenRequest, BodyHandlers.ofString());
+    }
+
+    private HttpRequest buildAccessTokenRequest(final String requestBody) {
+        return HttpRequest.newBuilder(toURI(accessTokenUrl))
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody)).build();
+    }
+
+    private URI toURI(final String accessTokenUrl) {
+        try {
+            return new URI(accessTokenUrl);
+        } catch (URISyntaxException e) {
+            throw new OauthProcessingException();
+        }
+    }
+
+    private String parseAccessToken(final HttpResponse<String> response) {
+        validateStatusSuccess(response);
+        final GitHubTokenResponse gitHubTokenResponse = parseJson(response, GitHubTokenResponse.class);
+        return gitHubTokenResponse.getAccessToken();
+    }
+
+    private void validateStatusSuccess(final HttpResponse<String> response) {
+        final HttpStatus status = HttpStatus.resolve(response.statusCode());
+        if (status == null || status.is4xxClientError()) {
+            throw new InvalidGitHubLoginException();
+        }
+        if (status.is5xxServerError()) {
+            throw new GitHubServerException();
+        }
+    }
+
+    private <T> T parseJson(final HttpResponse<String> response, Class<T> clazz) {
+        try {
+            return objectMapper.readValue(response.body(), clazz);
+        } catch (JsonProcessingException e) {
+            throw new OauthProcessingException();
+        }
     }
 
     public GitHubProfileResponse getProfile(final String accessToken) {
-        final WebClient webClient = WebClient.builder()
-                .baseUrl(profileUrl)
-                .defaultHeader(HttpHeaders.AUTHORIZATION, "token " + accessToken)
-                .build();
-        return requestProfile(webClient);
+        try {
+            final HttpResponse<String> profileResponse = requestProfile(accessToken);
+            return parseProfile(profileResponse);
+        } catch (IOException e) {
+            throw new OauthProcessingException();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new OauthProcessingException();
+        }
     }
 
-    private GitHubProfileResponse requestProfile(final WebClient webClient) {
-        return webClient.get()
-                .retrieve()
-                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
-                    throw new InvalidGitHubLoginException();
-                })
-                .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
-                    throw new GitHubServerException();
-                })
-                .bodyToMono(GitHubProfileResponse.class)
-                .blockOptional()
-                .orElseThrow(InvalidGitHubLoginException::new);
+    private HttpResponse<String> requestProfile(final String accessToken) throws IOException, InterruptedException {
+        final HttpRequest profileRequest = buildProfileRequest(accessToken);
+        return httpClient.send(profileRequest, BodyHandlers.ofString());
+    }
+
+    private HttpRequest buildProfileRequest(final String accessToken) {
+        return HttpRequest.newBuilder(toURI(profileUrl)).GET().header(HttpHeaders.AUTHORIZATION, "token " + accessToken)
+                .build();
+    }
+
+    private GitHubProfileResponse parseProfile(final HttpResponse<String> profileResponse) {
+        validateStatusSuccess(profileResponse);
+        return parseJson(profileResponse, GitHubProfileResponse.class);
     }
 }
