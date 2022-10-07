0b304bda0281de4e30673dba0e155fcafde18f67
tiki <78459713+yh20studio@users.noreply.github.com>
2022-10-07 15:26:40 +0900

    [Bug-fix/FE] dev 환경 github client id 변경 (#726)
    [Bug-fix/FE] dev 환경 github client id 변경 (#725)
    
    fix: dev 환경 github client id 변경

--- frontend/webpack.test.js
+++ frontend/webpack.test.js
@@ -7,7 +7,7 @@ module.exports = merge(common, {
   plugins: [
     new DefinePlugin({
       __API_URL__: JSON.stringify('https://dev.f12.app/api/v1'),
-      __GITHUB_CLIENT_ID__: JSON.stringify('f1e73a9ac502f1b6712a'),
+      __GITHUB_CLIENT_ID__: JSON.stringify('100bf58e7ab5aab7878e'),
     }),
   ],
 });
