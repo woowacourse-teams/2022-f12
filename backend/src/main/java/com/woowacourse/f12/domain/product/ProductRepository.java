f2c4253533dfb5dd88b86b9627b2baa246fa938d
yoon <duddbs774@gmail.com>
2022-10-12 01:12:34 +0900

    refactor: 서브쿼리 적용

--- backend/src/main/java/com/woowacourse/f12/domain/product/ProductRepository.java
+++ backend/src/main/java/com/woowacourse/f12/domain/product/ProductRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 
-    @Query(value = "select * from Product where category = :category and review_count >= :reviewCount and avg_rating >= :rating order by rand() limit :rowCount", nativeQuery = true)
+    @Query(value = "select * from product as i join "
+            + "(select id from product where category = :category and review_count >= :reviewCount and avg_rating >= :rating "
+            + "order by rand() limit :rowCount) "
+            + "as ids on i.id = ids.id",
+            nativeQuery = true)
     List<Product> findPopularProductsByRandom(String category, int reviewCount, double rating,
                                               int rowCount);
 }
