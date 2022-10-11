7a73775fbb63ed285a6a3a9dcee3eaf08da523d9
yoon <duddbs774@gmail.com>
2022-10-11 23:20:57 +0900

    feat: 카테고리, 리뷰 개수, 평점, 튜플 개수로 랜덤하게 제품을 조회한다.

--- backend/src/main/java/com/woowacourse/f12/domain/product/ProductRepository.java
+++ backend/src/main/java/com/woowacourse/f12/domain/product/ProductRepository.java
@@ -1,7 +1,12 @@
 package com.woowacourse.f12.domain.product;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 
+    @Query(value = "select * from Product where category = :category and review_count >= :reviewCount and avg_rating >= :rating order by rand() limit :rowCount", nativeQuery = true)
+    List<Product> findPopularProductsByRandom(String category, int reviewCount, double rating,
+                                              int rowCount);
 }
