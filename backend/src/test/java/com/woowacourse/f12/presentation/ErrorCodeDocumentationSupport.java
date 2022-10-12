3b632ed8007bfec862ef11bf646e757710ce0d3a
yangdongjue5510 <djwhy5510@gmail.com>
2022-10-12 15:32:29 +0900

    test: ErrorCode를 문서화를 도와주는 Support 클래스 구현

---
+++ backend/src/test/java/com/woowacourse/f12/presentation/ErrorCodeDocumentationSupport.java
@@ -0,0 +1,25 @@
+package com.woowacourse.f12.presentation;
+
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+
+import com.woowacourse.f12.exception.ErrorCode;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
+
+public class ErrorCodeDocumentationSupport {
+
+    public static ResponseFieldsSnippet makeErrorCodeSnippets(ErrorCode... errorCodes) {
+        return responseFields(
+                fieldWithPath("errorCode").description(codesToString(errorCodes)),
+                fieldWithPath("message").ignored());
+    }
+
+    private static String codesToString(ErrorCode... errorCodes) {
+        final StringBuilder builder = new StringBuilder();
+        for (ErrorCode code : errorCodes) {
+            builder.append(code.getValue()).append(", ");
+        }
+        builder.delete(builder.length() - 2, builder.length() - 1);
+        return builder.toString();
+    }
+}
