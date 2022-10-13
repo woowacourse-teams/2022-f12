0e487a933a445f8d22b2536b4ba400a03b788e37
yoon <duddbs774@gmail.com>
2022-10-13 10:55:09 +0900

    refactor: QueryDSL 대신 쿼리 메소드 기능 사용

--- backend/src/main/java/com/woowacourse/f12/domain/product/ProductRepositoryCustom.java
+++ backend/src/main/java/com/woowacourse/f12/domain/product/ProductRepositoryCustom.java
@@ -1,6 +1,5 @@
 package com.woowacourse.f12.domain.product;
 
-import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
@@ -9,6 +8,4 @@ public interface ProductRepositoryCustom {
     Slice<Product> findWithoutSearchConditions(Pageable pageable);
 
     Slice<Product> findWithSearchConditions(String keyword, Category category, Pageable pageable);
-
-    List<Product> findByReviewCountAndRatingGreaterThanEqual(int reviewCount, double rating);
 }
