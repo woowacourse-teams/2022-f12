2eb4d1e992059cfbffe1dc1bef0af8dfad1b17b4
yoon <duddbs774@gmail.com>
2022-10-13 10:55:09 +0900

    refactor: QueryDSL 대신 쿼리 메소드 기능 사용

--- backend/src/main/java/com/woowacourse/f12/domain/product/ProductRepository.java
+++ backend/src/main/java/com/woowacourse/f12/domain/product/ProductRepository.java
@@ -1,7 +1,9 @@
 package com.woowacourse.f12.domain.product;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long>, ProductRepositoryCustom {
 
+    List<Product> findByReviewCountAndRatingGreaterThanEqual(int reviewCount, double rating);
 }
