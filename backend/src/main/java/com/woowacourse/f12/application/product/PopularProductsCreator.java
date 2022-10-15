951149990f80a43b95061fdd3f75daae10dbe590
yoon <duddbs774@gmail.com>
2022-10-15 15:04:32 +0900

    feat: 인기 제품의 개수에 대한 유효성 검증 추가

--- backend/src/main/java/com/woowacourse/f12/application/product/PopularProductsCreator.java
+++ backend/src/main/java/com/woowacourse/f12/application/product/PopularProductsCreator.java
@@ -1,6 +1,7 @@
 package com.woowacourse.f12.application.product;
 
 import com.woowacourse.f12.domain.product.Product;
+import com.woowacourse.f12.exception.badrequest.InvalidPopularProductsSizeException;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.BiFunction;
@@ -11,20 +12,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class PopularProductsCreator {
 
+    private static final int PRODUCTS_MINIMUM_SIZE = 1;
+
     private final int reviewCount;
     private final double rating;
+    private final int maxSize;
 
     public PopularProductsCreator(@Value("${review-count}") final int reviewCount,
-                                  @Value("${rating}") final double rating) {
+                                  @Value("${rating}") final double rating,
+                                  @Value("${products-maximum-size}") final int maxSize) {
         this.reviewCount = reviewCount;
         this.rating = rating;
+        this.maxSize = maxSize;
     }
 
     public List<Product> create(final int size, final BiFunction<Integer, Double, List<Product>> biFunction) {
+        checkSize(size);
         final List<Product> products = biFunction.apply(reviewCount, rating);
         Collections.shuffle(products);
         return products.stream()
                 .limit(size)
                 .collect(Collectors.toList());
+    }
+
+    private void checkSize(final int size) {
+        if (size < PRODUCTS_MINIMUM_SIZE || size > maxSize) {
+            throw new InvalidPopularProductsSizeException(PRODUCTS_MINIMUM_SIZE, maxSize);
+        }
     }
 }
