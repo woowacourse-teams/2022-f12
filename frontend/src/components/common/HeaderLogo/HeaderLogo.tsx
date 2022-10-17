725d9ce39fabce37287e13bd14b519b18a6a1a00
Yo Wook Kim <uk960214@gmail.com>
2022-10-17 15:10:35 +0900

    [FE] 상하단 메뉴바 접근성 개선 (#777)
    * feat: 하단 메뉴바에 접근성을 위해 목록 추가, 레이블 추가
    
    * feat: 상단 메뉴바에 프로필 버튼 레이블 추가
    
    * refactor: 모바일 상단 메뉴바 컴포넌트 위치 headerNav로 이동, 레이블 추가

--- frontend/src/components/common/HeaderLogo/HeaderLogo.tsx
+++ frontend/src/components/common/HeaderLogo/HeaderLogo.tsx
@@ -1,13 +1,8 @@
 import * as S from '@/components/common/HeaderLogo/HeaderLogo.style';
 
-import useAuth from '@/hooks/useAuth';
-
-import { GITHUB_AUTH_URL } from '@/constants/api';
 import ROUTES from '@/constants/routes';
 
 import HeaderLogoImage from '@/assets/HeaderLogo.svg';
-import HeaderLogoImageHorizontal from '@/assets/HeaderLogoHorizontal.svg';
-import Profile from '@/assets/profile.svg';
 
 function HeaderLogo() {
   return (
@@ -18,27 +13,5 @@ function HeaderLogo() {
     </S.Container>
   );
 }
-
-function Mobile() {
-  const { isLoggedIn } = useAuth();
-  return (
-    <S.Container>
-      <S.LogoLink to={ROUTES.HOME}>
-        <HeaderLogoImageHorizontal />
-      </S.LogoLink>
-      {isLoggedIn ? (
-        <S.CustomLink to={ROUTES.MY_PROFILE}>
-          <Profile />
-        </S.CustomLink>
-      ) : (
-        <S.CustomLink as={'a'} href={GITHUB_AUTH_URL}>
-          로그인
-        </S.CustomLink>
-      )}
-    </S.Container>
-  );
-}
-
-HeaderLogo.Mobile = Mobile;
 
 export default HeaderLogo;
