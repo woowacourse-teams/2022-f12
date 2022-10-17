586434a295f9cf8c2eedb8db9636305ba26df616
Yo Wook Kim <uk960214@gmail.com>
2022-10-17 16:22:45 +0900

    [FE] 제품 목록 불러오기 실패나 항목 없을 때 스크린 리더 alert 추가 (#783)
    feat: 제품 목록 불러오기 실패나 항목 없을 때 스크린 리더 alert 추가

--- frontend/src/components/common/ErrorPlaceholder/ErrorPlaceholder.tsx
+++ frontend/src/components/common/ErrorPlaceholder/ErrorPlaceholder.tsx
@@ -3,12 +3,19 @@ import errorWebp from '@/assets/error.webp';
 
 function ErrorPlaceholder() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '1rem',
+      }}
+    >
       <picture>
         <source srcSet={errorWebp} type="image/webP" />
         <img src={errorGif} alt="" width="150" />
       </picture>
-      <div>오류가 발생했어요..</div>
+      <div role="alert">오류가 발생했어요..</div>
     </div>
   );
 }
