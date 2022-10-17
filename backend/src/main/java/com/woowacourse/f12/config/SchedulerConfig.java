0e4bd5f5c33c170bcb7cf8124efd6bdf74e665e7
Ohzzi <clover7even@naver.com>
2022-10-17 21:06:16 +0900

    fix: 스케줄러가 테스트 환경에서는 빈으로 등록되지 않도록 수정

--- backend/src/main/java/com/woowacourse/f12/config/SchedulerConfig.java
+++ backend/src/main/java/com/woowacourse/f12/config/SchedulerConfig.java
@@ -4,10 +4,12 @@ import com.woowacourse.f12.application.batch.BatchService;
 import com.woowacourse.f12.application.batch.FollowerCountBatchScheduler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @Configuration
+@Profile({"main", "release", "performance"})
 public class SchedulerConfig {
 
     private final BatchService batchService;
