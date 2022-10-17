67d9bf29cf3524e1e19354580a8bfbe253df29d1
jswith <typingjs@gmail.com>
2022-10-17 21:30:27 +0900

    feat: 페이지 이동 시 스크린리더로 안내

--- frontend/src/pages/common/PageLayout/PageLayout.tsx
+++ frontend/src/pages/common/PageLayout/PageLayout.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useEffect, useState } from 'react';
+import { Suspense, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 
 import * as S from '@/pages/common/PageLayout/PageLayout.style';
@@ -11,6 +11,8 @@ import Loading from '@/components/common/Loading/Loading';
 
 import useAuth from '@/hooks/useAuth';
 import useDevice from '@/hooks/useDevice';
+
+import { SROnly } from '@/style/GlobalStyles';
 
 function PageLayout() {
   const location = useLocation();
@@ -45,8 +47,33 @@ function PageLayout() {
 
   const { device } = useDevice();
 
+  const divRef = useRef<HTMLDivElement>(null);
+  const isInitialRender = useRef<boolean>(true);
+
+  const [pathName, setPathName] = useState<string>('');
+
+  useLayoutEffect(() => {
+    if (isInitialRender.current) {
+      isInitialRender.current = false;
+      return;
+    }
+
+    divRef.current?.focus();
+
+    setPathName(location.pathname.split('/')[1]);
+  }, [location.pathname]);
+
+  const pathNames = {
+    '': '홈 페이지로 이동합니다.',
+    products: '제품 검색 페이지로 이동합니다.',
+    product: '제품 상세 페이지로 이동합니다.',
+    profiles: '프로필 검색 페이지로 이동합니다.',
+    profile: '프로필 상세 페이지로 이동합니다.',
+  };
+
   return (
-    <>
+    <div tabIndex={-1} ref={divRef}>
+      <SROnly aria-live="assertive">{pathNames[pathName]}</SROnly>
       {device !== 'desktop' && <HeaderNav.Mobile />}
 
       {device === 'desktop' && (
@@ -63,7 +90,7 @@ function PageLayout() {
         </S.Main>
       </Suspense>
       {device !== 'desktop' && <BottomNavigation />}
-    </>
+    </div>
   );
 }
 
