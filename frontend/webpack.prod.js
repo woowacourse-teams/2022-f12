e88ee94cd072ae239bf1d8cf5361f7f50eccef3f
Yo Wook Kim <uk960214@gmail.com>
2022-10-18 21:40:20 +0900

    [Feature/FE] 프로젝트 레벨 이슈 수정 (#817)
    * [FE] 불필요한 스타일 관련 패키지를 제거 (#803)
    
    chore: 불필요한 스타일 관련 패키지를 제거
    
    * [FE] test 서버는 검색엔진에서 검색되지 않도록 robot 메타 태그 추가 (#805)
    
    * [FE] document title을 설정하고 구글 애널리틱스를 추가 (#808)
    
    * feat: 페이지 정보를 document title에 반영하도록 구현
    
    * fix: 제품 검색 페이지에서 default 값이 제대로 읽히지 않는 오류 해결
    
    * chore: 구글 애널리틱스 추가
    
    * [FE] manifest, favicon 관련 빌드 설정 업데이트 (#811)
    
    chore: manifest, favicon 관련 빌드 설정 업데이트

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,4 +1,6 @@
 const { DefinePlugin } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
@@ -6,6 +8,9 @@ module.exports = merge(common, {
   entry: './src/index.tsx',
   mode: 'production',
   plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+    }),
     new DefinePlugin({
       __API_URL__: JSON.stringify('https://prod.f12.app/api/v1'),
       __GITHUB_CLIENT_ID__: JSON.stringify('e77cbdeefd706dcff3f0'),
