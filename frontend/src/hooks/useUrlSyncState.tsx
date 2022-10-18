f8376a51481c0977a657ad7de4044c260f1c50fa
Yo Wook Kim <uk960214@gmail.com>
2022-10-18 15:36:50 +0900

    [FE] document title을 설정하고 구글 애널리틱스를 추가 (#808)
    * feat: 페이지 정보를 document title에 반영하도록 구현
    
    * fix: 제품 검색 페이지에서 default 값이 제대로 읽히지 않는 오류 해결
    
    * chore: 구글 애널리틱스 추가

--- frontend/src/hooks/useUrlSyncState.tsx
+++ frontend/src/hooks/useUrlSyncState.tsx
@@ -5,12 +5,13 @@ const useUrlSyncState = <T extends string>(
   key: string,
   defaultValue?: T
 ): [T, React.Dispatch<React.SetStateAction<T>>] => {
+  const realDefaultValue = defaultValue || null;
   const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const initialState = searchParams.get(key)
     ? decodeURIComponent(searchParams.get(key))
-    : defaultValue;
+    : realDefaultValue;
 
   // TODO: 만약 url의 초기값이 T가 아닌 경우를 대비해서 throw를 하고 ErrorBoundary를 설정할 것을 추후에 고민
   const [value, setValue] = useState<T>(initialState as T);
@@ -18,7 +19,7 @@ const useUrlSyncState = <T extends string>(
   // setState가 아니라 뒤로가기 등 상황에서 UI 동기화
   // 컴포넌트가 navigate 되지 않아 처리해주지 않으면 초기값 지정이 자동으로 되지 않음
   useEffect(() => {
-    setValue((searchParams.get(key) as T) || defaultValue);
+    setValue((searchParams.get(key) as T) || realDefaultValue);
   }, [location.key]);
 
   useEffect(() => {
