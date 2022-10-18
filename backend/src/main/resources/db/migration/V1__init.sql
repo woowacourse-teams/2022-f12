96738eb988a6ecd392b5ee1fde828888757b7f83
corinne <yoo77hyeon@gmail.com>
2022-10-18 15:18:35 +0900

    refactor: init sql type 변경

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -1,72 +1,83 @@
-DROP TABLE IF EXISTS `following`;
-CREATE TABLE `following`
+CREATE TABLE IF NOT EXISTS `following`
 (
-    `id`           bigint NOT NULL AUTO_INCREMENT,
-    `created_at`   datetime(6) NOT NULL,
-    `follower_id`  bigint NOT NULL,
-    `following_id` bigint NOT NULL,
+    `id`           bigint    NOT NULL AUTO_INCREMENT,
+    `created_at`   TIMESTAMP NOT NULL,
+    `follower_id`  bigint,
+    `following_id` bigint    NOT NULL,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `following_following_id_follower_id_unique` (`following_id`,`follower_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    UNIQUE KEY `following_following_id_follower_id_unique` (`follower_id`, `following_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `inventory_product`;
-CREATE TABLE `inventory_product`
+CREATE TABLE IF NOT EXISTS `inventory_product`
 (
     `id`         bigint NOT NULL AUTO_INCREMENT,
-    `selected`   bit(1) DEFAULT 0,
-    `member_id`  bigint NOT NULL DEFAULT NULL,
-    `product_id` bigint NOT NULL DEFAULT NULL,
+    `selected`   TINYINT(1) DEFAULT NULL,
+    `member_id`  bigint     DEFAULT NULL,
+    `product_id` bigint     DEFAULT NULL,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `inventory_member_product_unique` (`member_id`,`product_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    UNIQUE KEY `inventory_member_product_unique` (`member_id`, `product_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `member`;
-CREATE TABLE `member`
+CREATE TABLE IF NOT EXISTS `member`
 (
-    `id`             bigint        NOT NULL AUTO_INCREMENT,
-    `career_level`   varchar(255) DEFAULT NULL,
-    `follower_count` int           NOT NULL DEFAULT 0,
-    `github_id`      varchar(255)  NOT NULL,
-    `image_url`      varchar(15000) NOT NULL,
-    `job_type`       varchar(255) DEFAULT NULL,
-    `name`           varchar(255) DEFAULT NULL,
-    `registered`     bit(1)        NOT NULL DEFAULT 0,
-    `role`           varchar(255)  NOT NULL DEFAULT 'USER',
+    `id`             bigint         NOT NULL AUTO_INCREMENT,
+    `career_level`   varchar(255)            DEFAULT NULL,
+    `follower_count` int            NOT NULL DEFAULT 0,
+    `github_id`      varchar(255)   NOT NULL DEFAULT NULL,
+    `image_url`      varchar(15000) NOT NULL DEFAULT NULL,
+    `job_type`       varchar(255)            DEFAULT NULL,
+    `name`           varchar(255)            DEFAULT NULL,
+    `registered`     TINYINT(1)     NOT NULL DEFAULT 0,
+    `role`           varchar(255)    NOT NULL DEFAULT 'USER',
     PRIMARY KEY (`id`),
     UNIQUE KEY `github_id_unique` (`github_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `product`;
-CREATE TABLE `product`
+CREATE TABLE IF NOT EXISTS `product`
 (
     `id`           bigint         NOT NULL AUTO_INCREMENT,
-    `category`     varchar(255)     NOT NULL,
-    `image_url`    varchar(15000) NOT NULL,
-    `name`         varchar(255)   NOT NULL,
+    `category`     varchar(255)            DEFAULT NULL,
+    `image_url`    varchar(15000) NOT NULL DEFAULT NULL,
+    `name`         varchar(255)   NOT NULL DEFAULT NULL,
     `avg_rating`   double         NOT NULL DEFAULT 0,
     `review_count` int            NOT NULL DEFAULT 0,
     `total_rating` int            NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`),
     UNIQUE KEY `NAME_UNIQUE` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `review`;
-CREATE TABLE `review`
+CREATE TABLE IF NOT EXISTS `review`
 (
     `id`         bigint        NOT NULL AUTO_INCREMENT,
-    `content`    varchar(1000) NOT NULL,
-    `created_at` datetime(6) NOT NULL,
-    `rating`     int           NOT NULL,
-    `member_id`  bigint        NOT NULL,
-    `product_id` bigint        NOT NULL,
+    `content`    varchar(1000) NOT NULL DEFAULT NULL,
+    `created_at` TIMESTAMP     NOT NULL DEFAULT NULL,
+    `rating`     int           NOT NULL DEFAULT NULL,
+    `member_id`  bigint        NOT NULL DEFAULT NULL,
+    `product_id` bigint        NOT NULL DEFAULT NULL,
     PRIMARY KEY (`id`),
-    UNIQUE KEY `review_product_id_member_id_unique` (`product_id`,`member_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+    UNIQUE KEY `review_product_id_member_id_unique` (`member_id`, `product_id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
 
-DROP TABLE IF EXISTS `refresh_token`;
-create table `refresh_token`
+CREATE TABLE IF NOT EXISTS `refresh_token`
 (
     `token_value` varchar(50) primary key,
-    `expired_at`  timestamp not null,
+    `expired_at`  TIMESTAMP not null,
     `member_id`   bigint    not null
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_0900_ai_ci;
