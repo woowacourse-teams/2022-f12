6f9b73919ba6274d61026b6e645166a6f5a05f9d
Yo Wook Kim <uk960214@gmail.com>
2022-10-27 10:43:38 +0900

    fix: MSW 설정 복구

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -15,17 +15,17 @@ import theme from '@/style/theme';
 
 /* eslint-disable */
 
-// if (process.env.NODE_ENV === 'development' && !window.Cypress) {
-//   const { worker } = require('@/mocks/browser');
-//   worker.start({
-//     onUnhandledRequest(req) {
-//       const urlPath = req.url.pathname;
+if (process.env.NODE_ENV === 'development' && !window.Cypress) {
+  const { worker } = require('@/mocks/browser');
+  worker.start({
+    onUnhandledRequest(req) {
+      const urlPath = req.url.pathname;
 
-//       if (!urlPath.startsWith('http://localhost:8080')) return;
-//       console.warn('Found an unhandled %s request to %s', req.method, req.url.href);
-//     },
-//   });
-// }
+      if (!urlPath.startsWith('http://localhost:8080')) return;
+      console.warn('Found an unhandled %s request to %s', req.method, req.url.href);
+    },
+  });
+}
 
 /* eslint-enable */
 
