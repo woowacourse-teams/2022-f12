4d9872a6faa8aa5b643fd86efcf2040836a40ae0
Yo Wook Kim <uk960214@gmail.com>
2022-12-28 11:54:07 +0900

    refactor: 기존 캐시 관련 로직에서 불필요한 부분 제거

--- frontend/src/contexts/CacheContextProvider.tsx
+++ frontend/src/contexts/CacheContextProvider.tsx
@@ -4,30 +4,18 @@ import { createContext, PropsWithChildren, useState } from 'react';
 import { CACHE_TIME } from '@/constants/cache';
 
 type CacheBody = {
-  response: AxiosResponse<unknown> | AxiosResponse<unknown>[];
+  response: AxiosResponse<unknown>;
   expires: number;
 };
-type CacheMap = Map<string, CacheBody | CacheBody[]>;
+type CacheMap = Map<string, CacheBody>;
 
 export const GetCacheContext = createContext<
-  | null
-  | ((
-      url: string,
-      page?: number
-    ) => AxiosResponse<unknown, unknown> | AxiosResponse<unknown, unknown>[] | undefined)
+  null | ((url: string) => AxiosResponse<unknown, unknown> | undefined)
 >(null);
 export const AddCacheContext = createContext<
   null | ((url: string, response: AxiosResponse<unknown>, maxAge?: number) => void)
 >(null);
-export const AddCacheArrayContext = createContext<
-  | null
-  | ((
-      url: string,
-      page: number,
-      response: AxiosResponse<unknown>,
-      maxAge?: number
-    ) => void)
->(null);
+
 export const RemoveCacheContext = createContext<null | ((url: string) => void)>(null);
 
 function CacheContextProvider({ children }: PropsWithChildren) {
@@ -41,26 +29,6 @@ function CacheContextProvider({ children }: PropsWithChildren) {
     setCache((prev) => prev.set(url, { response, expires: Date.now() + maxAge }));
   };
 
-  const addCacheArray = (
-    url: string,
-    page: number,
-    response: AxiosResponse<unknown>,
-    maxAge = CACHE_TIME.ONE_MINS
-  ) => {
-    setCache((prev) => {
-      const prevCache = prev.get(url) || [];
-      if (!(prevCache instanceof Array)) {
-        throw new Error('잘못된 캐시 저장입니다.');
-      }
-      prevCache[page] = { response, expires: Date.now() + maxAge };
-      return prev.set(url, prevCache);
-    });
-  };
-
-  const isCacheBodyArray = (
-    input: CacheBody | CacheBody[] | undefined
-  ): input is CacheBody[] => Array.isArray(input);
-
   const handleCacheExpiry = (cache: CacheBody, url: string) => {
     if (cache.expires < Date.now()) {
       removeCache(url);
@@ -70,25 +38,13 @@ function CacheContextProvider({ children }: PropsWithChildren) {
     return cache.response;
   };
 
-  const getCache = (url: string, page?: number) => {
+  const getCache = (url: string) => {
     const cacheBody = cache.get(url);
-    const isCacheArray = isCacheBodyArray(cacheBody);
     if (cacheBody === undefined) {
       return;
     }
-    if (!isCacheArray) {
-      return handleCacheExpiry(cacheBody, url);
-    }
-    if (page === undefined) {
-      throw new Error('페이지를 포함해서 캐시를 조회해야 합니다.');
-    }
-    if (cacheBody[page] === undefined) {
-      return;
-    }
 
-    const pageCacheBody = cacheBody[page];
-
-    return handleCacheExpiry(pageCacheBody, url);
+    return handleCacheExpiry(cacheBody, url);
   };
 
   const removeCache = (url: string) => {
@@ -102,11 +58,9 @@ function CacheContextProvider({ children }: PropsWithChildren) {
   return (
     <GetCacheContext.Provider value={getCache}>
       <AddCacheContext.Provider value={addCache}>
-        <AddCacheArrayContext.Provider value={addCacheArray}>
-          <RemoveCacheContext.Provider value={removeCache}>
-            {children}
-          </RemoveCacheContext.Provider>
-        </AddCacheArrayContext.Provider>
+        <RemoveCacheContext.Provider value={removeCache}>
+          {children}
+        </RemoveCacheContext.Provider>
       </AddCacheContext.Provider>
     </GetCacheContext.Provider>
   );
